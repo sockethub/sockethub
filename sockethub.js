2447c9d870f55064cc59ab1ca7fc51a81603f0b5
Nick Jennings <nick@silverbucket.net>
2013-01-15 23:11:30 +0700

    lots of renaming and re-organizing of the dispatcher process. the layout is still not completely stablized yet, and some additional validation still not finished, and left out of this commit.

--- sockethub.js
+++ sockethub.js
@@ -1,10 +1,16 @@
 module.exports = (function() {
   var config = require('./config.js').config;
 
-  var server = require('./lib/httpServer').init(config);
+  if (config.HOST.DISPATCHER) {
+    var server = require('./lib/httpServer').init(config);
 
-  // initialize websocket server
-  var wsServer = require('./lib/wsServer').init(config, server);
+    // initialize websocket server
+    var wsServer = require('./lib/wsServer').init(config, server);
+  }
+
+  for (var i = 0, len = config.HOST.MY_PLATFORMS.length; i < len; i = i + 1) {
+    console.log('... should load listener for '+config.HOST.MY_PLATFORMS[i]);
+  }
 
   console.log(' [*] finished loading' );
 }());
