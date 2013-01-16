91154047e2163793367584e8080a3c647646e487
Nick Jennings <nick@silverbucket.net>
2013-01-16 16:44:34 +0700

    moved dispatcher and listener into sockethub protocol dir for simplicity, added redis support - listeners and dispatchers now use it to communitcate, added subsystem which allows the dispatcher to ping the listeners to verify theyre up.

--- sockethub.js
+++ sockethub.js
@@ -1,18 +1,21 @@
 module.exports = (function() {
   var config = require('./config.js').config;
 
+  var listener, l;
+  if (config.HOST.MY_PLATFORMS.length > 0) {
+    listener = require('./lib/protocols/sockethub/listener');
+  }
+  for (var i = 0, len = config.HOST.MY_PLATFORMS.length; i < len; i = i + 1) {
+    //console.log('initializing listener for '+config.HOST.MY_PLATFORMS[i]);
+    l  = Object.create(listener);
+    l.init(config.HOST.MY_PLATFORMS[i]);
+  }
+
   if (config.HOST.DISPATCHER) {
     var server = require('./lib/httpServer').init(config);
 
     // initialize websocket server
     var wsServer = require('./lib/wsServer').init(config, server);
-  }
-
-  var listener = require('./lib/protocols/listener');
-  for (var i = 0, len = config.HOST.MY_PLATFORMS.length; i < len; i = i + 1) {
-    //console.log('initializing listener for '+config.HOST.MY_PLATFORMS[i]);
-    var l  = Object.create(listener);
-    l.init(config.HOST.MY_PLATFORMS[i]);
   }
 
   console.log(' [*] finished loading' );
