94638a9203fb316c6317155afae1ea98538a1134
() <nilclass@riseup.net>
2013-01-22 12:07:35 +0700

    session tests for remoteStorage interaction

--- test/session-suite.js
+++ test/session-suite.js
@@ -122,9 +122,124 @@ define(['require'], function(require) {
           test.assertAnd(env.session.getSettings(), {});
           test.assert(env.session.getPlatforms(), []);
         }
+      },
+      {
+        desc: "Session#get returns a promise",
+        run: function(env, test) {
+          var promise = env.session.get('foo', 'bar');
+          test.assertTypeAnd(promise, 'object');
+          test.assertType(promise.then, 'function');
+        }
+      },
+      {
+        desc: "Session#get fails when it has no remoteStorage config",
+        run: function(env, test) {
+          var promise = env.session.get('foo', 'bar');
+          promise.then(function() {
+            test.result(false, "Expected Session#get to fail, but it succeeded");
+          }, function() {
+            test.result(true);
+          });
+        }
+      }
+    ]
+  });
+
+  suites.push({
+    name: "Session settings from remote",
+    desc: "Session interaction with remoteStorage",
+    setup: function(env, test) {
+
+      env.Session = require('../lib/protocols/sockethub/session');
+      env.sid = 'test-sid';
+
+      var http = require('http');
+      env.testServer = http.createServer(function(req, res) {
+        env.captured.push(req);
+        var r = env.simulateResponse;
+        res.writeHead(r[0], r[1]);
+        res.write(r[2]);
+        res.end();
+      });
+      env.testServer.listen(12345, function() {
+        test.result(true);
+      });
+    },
+    takedown: function(env, test) {
+      env.testServer.close(function() {
+        test.result(true);
+      });
+    },
+    beforeEach: function(env, test) {
+      env.session = env.Session.get(env.sid);
+      env.session.configure({
+        storageInfo: {
+          href: 'http://localhost:12345/storage',
+        },
+        bearerToken: 'test-token'
+      });
+      env.captured = [];
+      env.simulateResponse = [200, { 'Content-Type': 'text/plain'}, 'Hello World'];
+      test.result(true);
+    },
+    afterEach: function(env, test) {
+      env.Session.destroy(env.sid);
+      test.result(true);
+    },
+    tests: [
+      {
+        desc: "Session#get sends a request",
+        run: function(env, test) {
+          return env.session.get('foo', 'bar').
+            then(function() {
+              test.assert(env.captured.length, 1);
+            });
+        }
+      },
+      {
+        desc: "Session#get builds the path based on storage-root, module and path",
+        run: function(env, test) {
+          return env.session.get('foo', 'bar').
+            then(function() {
+              var req = env.captured[0];
+              test.assert(req.url, '/storage/foo/bar');
+            });
+        }
+      },
+      {
+        desc: "Session#get sets the Authorization header correctly",
+        run: function(env, test) {
+          return env.session.get('phu', 'quoc').
+            then(function() {
+              var req = env.captured[0];
+              test.assert(req.headers['authorization'], 'Bearer test-token');
+            });
+        }
+      },
+      {
+        desc: "Session#get yields the response body and MIME type",
+        run: function(env, test) {
+          return env.session.get('foo', 'bar').
+            then(function(result) {
+              test.assertAnd(result.mimeType, 'text/plain');
+              test.assert(result.data, 'Hello World');
+            });
+        }
+      },
+      {
+        desc: "Session#get unpacks JSON data",
+        run: function(env, test) {
+          env.simulateResponse = [200, { 'Content-Type': 'application/json' },
+                                  '{"phu":"quoc"}'];
+          return env.session.get('foo', 'baz').
+            then(function(result) {
+              test.assert(result, { phu: 'quoc' });
+            });
+        }
       }
     ]
   });
 
   return suites;
 });
+
