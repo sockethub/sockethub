6a6a81e37a3bcf9dd0e4ef8943c22289b35c5dd6
Nick Jennings <nick@silverbucket.net>
2013-02-05 16:02:22 +0100

    updated listener to expect a promise from platform commands. for sending new updates from the platform, they should use the session.send() function, but to signify a direct response to a verb, they must fulfill a promise. also fixed email tests which now work

--- test/20-email-suite.js
+++ test/20-email-suite.js
@@ -1,37 +1,90 @@
 if(typeof(define) !== 'function') {
   var define = require('amdefine')(module);
 }
-define(['require'], function(require) {
+define(['require'], function (require) {
   var suites = [];
 
   suites.push({
-    name: "smtp platform tests",
-    desc: "collection of tests for the smtp platform",
-    setup: function(env, test) {
-      env.Mailer = {
+    name: "email platform tests",
+    desc: "collection of tests for the email platform",
+    setup: function (env, test) {
+      env.mailer = {
         send: this.Stub(function(obj, cb) {
+          console.log('MAILER STUB CALLED');
           cb(null, true);
         })
       };
-      env.Session = {
-        send: function(msg) {
-          //console.log(msg);
-        }
+      GLOBAL.mailer = env.mailer;
+      env.respHandler = function (testObj) {
+        return function(err, status, obj) {
+          if (testObj !== undefined) {
+            testObj.write(' responseHandler: ['+err+'] ['+status+'] ['+obj+']');
+            testObj.result(status);
+          } else {
+            test.write(' responseHandler: ['+err+'] ['+status+'] ['+obj+']');
+          }
+        };
       };
-      env.Smtp = require('../lib/protocols/sockethub/platforms/smtp-implementation');
-      test.result(true);
+
+      var Session = require('../lib/protocols/sockethub/session');
+      Session.get('testsess1').
+        then(function (session) {
+          env.session = session;
+
+          return session.getPlatformSession('email');
+        }).
+        then(function (psession) {
+          env.psession = psession;
+          env.psession.send = function (job) {
+            test.write('psession send called:',job);
+          };
+          env.Email = require('../lib/protocols/sockethub/platforms/email');
+          //console.log('email:', env.Email);
+          test.result(true);
+        });
     },
     tests: [
       {
-        desc: "Smtp.message() calls mailer.send()",
-        run: function(env, test) {
-          env.Smtp.message({
-            actor: { address: 'whitney@houston.com' },
+        desc: "Subscribe to platform with credential details",
+        run: function (env, test) {
+          var job = {
+            rid: '001',
+            verb: 'subscribe',
+            platform: 'email',
+            object: {
+              credentials: {
+                'whitney@houston.com': {
+                  smtp: {
+                    host: 'mailservice.example.com',
+                    username: 'whit',
+                    password: 'ney'
+                  }
+                }
+              }
+            }
+          };
+
+          env.psession.setConfig('credentials', job.object.credentials);
+          env.psession.getConfig('credentials').then(function (creds) {
+            test.assert(creds, job.object.credentials);
+          });
+        }
+      },
+      {
+        desc: "Email.message() calls mailer.send()",
+        run: function (env, test) {
+          var job = {
+            rid: '002',
+            verb: 'message',
+            platform: 'email',
+            actor: { name: 'Whitney Houston', address: 'whitney@houston.com' },
             object: { subject: 'Love you', text: 'I will always.' },
-            target: { to: [{ address: 'stevie@wonder.com' }] },
-            credentials: { host: 'example.com', user: 'whit', password: 'ney' }
-          }, env.Session);
-          test.assert(env.Mailer.called, undefined);//FIXME: why is this undefined?
+            target: { to: [{ name: 'Stevie Wonder', address: 'stevie@wonder.com' }] }
+          };
+          env.Email.message(job, env.psession).then(function (err, status, obj) {
+            env.respHandler()(err, status, obj);
+            test.assert(env.mailer.send.called, true);
+          });
         }
       }
     ]
