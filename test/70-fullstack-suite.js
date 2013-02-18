c0ce2342d863dd6ca387663848526f9b27ff81e6
Nick Jennings <nick@silverbucket.net>
2013-02-18 19:51:51 +0100

    fixed message from errMsg

--- test/70-fullstack-suite.js
+++ test/70-fullstack-suite.js
@@ -114,7 +114,7 @@ define(['require'], function (require) {
             rid: "123454",
             verb: 'send',
             platform: "email",
-            errMsg:"could not get remoteStorage for _internal platform"
+            message:"could not get remoteStorage for _internal platform"
           };
           env.connection.sendAndVerify(JSON.stringify(data), expected, test, env.confirmProps);
         }
