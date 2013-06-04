0438d08cee675c68bbb0ba8a859ef681b87105d6
Nick Jennings <nick@silverbucket.net>
2013-06-04 19:24:42 +0200

    moved feed displaying directives to root examples file

--- examples/modules/examples.js
+++ examples/modules/examples.js
@@ -123,7 +123,7 @@ function() {
 
 
 /**
- * Controller: navCtrl
+ * controller: navCtrl
  */
 controller('navCtrl',
 ['$scope', '$rootScope', '$location',
@@ -131,5 +131,101 @@ function navCtrl($scope, $rootScope, $location) {
   $scope.navClass = function (page) {
     var currentRoute = $location.path().substring(1) || 'home';
     return page === currentRoute.substr(0, page.length) ? 'active' : '';
+  };
+}]).
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// DIRECTIVES
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * directive: articles
+ */
+directive('articles', [
+function () {
+  return {
+    restrict: 'A',
+    scope: {
+      feeds: '='
+    },
+    template: '<div class="well" ng-repeat="f in feeds">' +
+              '  <h2>{{ f.object.title }}</h2>' +
+              '  <p>feed: <i>{{ f.actor.address }}</i></p>' +
+              '  <p>article link: <i><a target="_blank" href="{{ f.object.link }}">{{ f.object.link }}</a><i></p>' +
+              '  <div data-brief data-ng-bind-html-unsafe="f.object.brief_html"></div>' +
+              '</div>',
+    link: function (scope) {
+      console.log('ARTICLES: ', scope.feeds);
+      for (var i = 0, num = scope.feeds.length; i < num; i = i + 1) {
+        if (!scope.feeds[i].object.html) {
+          scope.feeds[i].object.html = scope.feeds[i].object.text;
+        }
+        if (!scope.feeds[i].object.brief_html) {
+          scope.feeds[i].object.brief_html = scope.feeds[i].object.brief_text;
+        }
+      }
+    }
+  };
+}]).
+
+
+/**
+ * directive: feedList
+ */
+directive('feedList', [
+function () {
+  return {
+    restrict: 'A',
+    scope: {
+      feeds: '='
+    },
+    template: '<h2 ng-transclude></h2>' +
+              '<ul class="nav nav-list">' +
+              '  <li ng-repeat="f in uniqueFeeds" data-toggle="tooltip" title="{{ f.address }}">' +
+              '    <a href="#/{{f.platform}}/feed/{{ f.address | urlEncode }}">{{ f.name }}</a>' +
+              '  </li>' +
+              '</ul>',
+    link: function (scope, element, attrs) {
+      scope.uniqueFeeds = [];
+
+      for (var i = 0, num = scope.feeds.length; i < num; i = i + 1) {
+        var match = false;
+        for (var j = 0, jnum = scope.uniqueFeeds.length; j < jnum; j = j + 1) {
+          if (scope.uniqueFeeds[j].address === scope.feeds[i].actor.address) {
+            match = true;
+            break;
+          }
+        }
+        if (!match) {
+          scope.uniqueFeeds.push({ address: scope.feeds[i].actor.address,
+                                   name: scope.feeds[i].actor.name,
+                                   description: scope.feeds[i].actor.description,
+                                   platform: scope.feeds[i].platform });
+        }
+      }
+      console.log('**** feeds: ', scope.feeds);
+      console.log('**** uniqueFeeds: ', scope.uniqueFeeds);
+    },
+    transclude: true
+  };
+}]).
+
+
+/**
+ * directive: brief
+ */
+directive('brief', [
+function () {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      //console.log('LINK: ', element[0]);
+    }
   };
 }]);
