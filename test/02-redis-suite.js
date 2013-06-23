035601d164a0a5f35e7123264b296686cbe80af0
Nick Jennings <nick@silverbucket.net>
2013-06-23 20:26:51 +0200

    updated redis tests

--- test/02-redis-suite.js
+++ test/02-redis-suite.js
@@ -19,7 +19,6 @@ define(['require'], function (require) {
       {
         desc: "verify redis is available",
         run: function (env, test) {
-          test.result(false);
           env.util.redisCheck(function(err) {
             if (err) {
               test.result(false, err);
