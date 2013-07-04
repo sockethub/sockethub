13e57e1d4bc4be9473f86e9938e85220ea6f476f
Nick Jennings <nick@silverbucket.net>
2013-07-04 19:47:19 +0200

    updated mock

--- test/mocks/redis-mock.js
+++ test/mocks/redis-mock.js
@@ -1,37 +1,47 @@
-var EventEmitter = require('events').EventEmitter;
+var instances = {};
 
 var Redis = function (test) {
   return {
-    createClient: function () {
-      var events = new EventEmitter();
-      var pub = {};
-      var subscribed_channels = [];
+    createClient: new test.Stub(function () {
+      var me = Math.floor((Math.random()*10)+1) + new Date().getTime();
+      instances[me] = {};
+      instances[me].subscribed_channels = [];
+      var events = {
+        'message': [],
+        'error': []
+      };
 
-      pub.subscribe = new test.Stub(function (channel) {
-        subscribed_channels.push(channel);
+      instances[me].subscribe = new test.Stub(function (channel) {
+        console.log('instance['+me+'] subscribing to: '+channel);
+        instances[me].subscribed_channels.push(channel);
       });
 
-      pub.publish = new test.Stub(function (channel, json) {
-
+      instances[me].publish = new test.Stub(function (channel, json) {
+        instances[me].__fireEvent(channel, 'message', json);
       });
 
-      pub.on = new test.Stub(function (event, callback) {
-        events.on(event, callback);
+      instances[me].on = new test.Stub(function (event, callback) {
+        console.log('instance['+me+'] registering callback for \''+event+'\'');
+        events[event].push(callback);
       });
 
-      pub.__fireEvent = function (channel, event, data, callback) {
-        if (subscribed_channels.indexOf(channel) > -1) {
-          if (callback) {
-            events.emit(event, data, callback);
-          } else {
-            events.emit(event, data);
+      instances[me].__fireEvent = function (channel, event, data) {
+        if (instances[me].subscribed_channels.indexOf(channel) > -1) {
+          console.log('instance['+me+'] sub. to chan. \''+channel+'\', send: \''+event+'\': '+data);
+          for (var i = 0, num = events[event].length; i < num; i = i + 1) {
+            events[event][i](channel, data);
           }
         } else {
-          console.log('no one subscribed to channel \''+channel+'\', not firing \''+event+'\'');
+          console.log('instance['+me+'] not subscribed to channel \''+channel+'\', not firing \''+event+'\'', instances[me].subscribed_channels);
         }
       };
 
-      return pub;
+      return instances[me];
+    }),
+    __fireEvent: function (channel, event, data) {
+      for (var key in instances) {
+        instances[key].__fireEvent(channel, event, data);
+      }
     }
   };
 };
