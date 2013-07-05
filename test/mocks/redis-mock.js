8390ad1e78f464173977f0d6e996e3af8e6562a9
Nick Jennings <nick@silverbucket.net>
2013-07-05 16:39:42 +0200

    improved tests and randomization for mock

--- test/mocks/redis-mock.js
+++ test/mocks/redis-mock.js
@@ -1,69 +1,82 @@
-var instances = {};
-
-var Redis = function (test) {
-
-  function __fireEvent (channel, event, data) {
-    for (var key in instances) {
-      instances[key].__fireEvent(channel, event, data);
+var Redis = {
+  __test: { Stub: function () {}},
+  __instances: {},
+  __fireEvent: function (channel, event, data) {
+    console.log('__fireEvent called, instances: ', this.__instances);
+    for (var key in this.__instances) {
+      console.log('REDIS MOCK EVENT FIRE: ',data);
+      this.__instances[key].__fireEvent(channel, event, data, key);
     }
-  }
+  },
+  __clearHandlers: function () {
+    for (var key in this.__instances) {
+      this.__instances[key].__clearHandlers();
+      delete this.__instances[key];
+    }
+    this.__instances = {};
+  },
+  createClient: function () {
+    _this = this;
+    var randa = Math.floor((Math.random()*10)+1) + new Date().getTime();
+    var randb = Math.floor((Math.random()*10)+2) *
+                Math.floor((Math.random()*10)+3) /
+                Math.floor((Math.random()*10)+4);
+    var me = randa * randb / 1000;
+    _this.__instances[me] = {};
+    _this.__instances[me].subscribed_channels = [];
+    var events = {
+      'message': [],
+      'error': []
+    };
 
-  return {
-    createClient: new test.Stub(function () {
-      var me = Math.floor((Math.random()*10)+1) / new Date().getTime();
-      instances[me] = {};
-      instances[me].subscribed_channels = [];
-      var events = {
-        'message': [],
-        'error': []
-      };
+    _this.__instances[me].subscribe = new _this.__test.Stub(function (channel) {
+      //console.log('instance['+me+'] subscribing to: '+channel);
+      _this.__instances[me].subscribed_channels.push(channel);
+    });
 
-      instances[me].subscribe = new test.Stub(function (channel) {
-        //console.log('instance['+me+'] subscribing to: '+channel);
-        instances[me].subscribed_channels.push(channel);
-      });
+    _this.__instances[me].publish = new _this.__test.Stub(function (channel, json) {
+      _this.__fireEvent(channel, 'message', json);
+    });
 
-      instances[me].publish = new test.Stub(function (channel, json) {
-        __fireEvent(channel, 'message', json);
-      });
+    _this.__instances[me].on = new _this.__test.Stub(function (event, callback) {
+      console.log('instance['+me+'] registering callback for \''+event+'\'');
+      events[event].push(callback);
+    });
 
-      instances[me].on = new test.Stub(function (event, callback) {
-        //console.log('instance['+me+'] registering callback for \''+event+'\'');
-        events[event].push(callback);
-      });
+    _this.__instances[me].__fireEvent = function (channel, event, data, key) {
+      if (!key) {
+        key = me;
+      }
+      if (!_this.__instances[key]) { return; }
 
-      instances[me].__fireEvent = function (channel, event, data) {
-        if (instances[me].subscribed_channels.indexOf(channel) > -1) {
-          console.log('instance['+me+'] chan. \''+channel+'\', send: \''+event+'\': '+data);
+      try {
+        if (_this.__instances[key].subscribed_channels.indexOf(channel) > -1) {
+          console.log('REDIS EVENT: instance['+key+'] chan. \''+channel+'\', send: \''+event+'\': '+data);
           for (var i = 0, num = events[event].length; i < num; i = i + 1) {
             events[event][i](channel, data);
           }
         } else {
-          ///console.log('instance['+me+'] not subscribed to channel \''+channel+'\', not firing \''+event+'\'', instances[me].subscribed_channels);
+          ///console.log('instance['+key+'] not subscribed to channel \''+channel+'\', not firing \''+event+'\'', instances[key].subscribed_channels);
         }
-      };
-
-      instances[me].__clearHandlers = function () {
-        for (var i = 0, num = events.length; i < num; i = i + 1) {
-          for (var j = 0, numj = events[i].length; j < numj; j = j + 1) {
-            delete events[i][j];
-          }
-          events[i] = {};
-        }
-        instances[me].subscribed_channels = [];
-      };
-
-      return instances[me];
-    }),
-    __fireEvent: __fireEvent,
-    __clearHandlers: function () {
-      for (var key in instances) {
-        instances[key].__clearHandlers();
-        delete instances[key];
+      } catch (e) {
+        throw new Error("failed reading indexOf for key:"+key+": ", e);
       }
-      instances = {};
-    }
-  };
-};
+    };
 
-module.exports = Redis;
+    _this.__instances[me].__clearHandlers = function () {
+      for (var i = 0, num = events.length; i < num; i = i + 1) {
+        for (var j = 0, numj = events[i].length; j < numj; j = j + 1) {
+          delete events[i][j];
+        }
+        events[i] = {};
+      }
+      _this.__instances[me].subscribed_channels = [];
+    };
+
+    return _this.__instances[me];
+  }
+};
+module.exports = function (t) {
+  Redis.__test = t;
+  return Redis;
+};
