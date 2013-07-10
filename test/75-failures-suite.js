4239d3782d582a932af0ef1bffc9ae581d6baede
Nick Jennings <nick@silverbucket.net>
2013-07-10 17:44:50 +0200

    added some tests for issue #81

---
+++ test/75-failures-suite.js
@@ -0,0 +1,108 @@
+require("consoleplusplus/console++");
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
+define(['require'], function (require) {
+  var suites = [];
+
+  suites.push({
+    desc: "listeners runing without dispatcher",
+    abortOnFail: true,
+    setup: function (env, test) {
+      //GLOBAL.redis = require('redis');
+      env.util = require('./../lib/sockethub/util');
+
+      env.confirmProps = {
+        status: true,
+        verb: 'confirm'
+      };
+
+      var port = 99550;
+
+      env.config = {};
+      env.config.HOST = {
+        ENABLE_TLS: false,
+        PORT: port,
+        MY_PLATFORMS: [ 'rss' ] // list of platforms this instance is responsible for
+      };
+
+      env.sockethubId = Math.floor((Math.random()*10)+1) + new Date().getTime() / Math.floor((Math.random()*10)+2);
+      env.encKey = Math.floor((Math.random()*10)+1) + new Date().getTime() / Math.floor((Math.random()*10)+2);
+      env.listener = [];
+      var proto = require('./../lib/sockethub/protocol');
+      listener = require('./../lib/sockethub/listener');
+      for (var i = 0, len = env.config.HOST.MY_PLATFORMS.length; i < len; i = i + 1) {
+        if (env.config.HOST.MY_PLATFORMS[i] === 'dispatcher') {
+          continue;
+        }
+        l  = listener();
+        l.init({
+          platform: proto.platforms[env.config.HOST.MY_PLATFORMS[i]],
+          sockethubId: env.sockethubId,
+          encKey: env.encKey
+        });
+        env.listener[i] = l;
+      }
+
+      test.result(true);
+    },
+    tests: [
+
+      {
+        desc: "send job to platform with an invalid tagret object",
+        run: function (env, test) {
+          env.sid = "1617171";
+          env.job_channel = 'sockethub:'+env.sockethubId+':listener:rss:incoming';
+          env.resp_channel = 'sockethub:'+env.sockethubId+':dispatcher:outgoing:'+env.sid;
+          var job = {
+            platform: 'rss',
+            verb: 'fetch',
+            actor: { address: 'johndoe'},
+            object: {},
+            target: { address: 'http://blog.silverbucket.net/rss'},
+            sessionId: env.sid
+          };
+
+          env.util.redis.set('lpush', env.job_channel, JSON.stringify(job));
+          env.util.redis.get('brpop', env.resp_channel, function (err, resp) {
+            var r = JSON.parse(resp[1]);
+            test.assertAnd(r.status, false);
+            test.assert(r.object.message, "Error: undefined is not a valid uri or options object.");
+          });
+        }
+      },
+      {
+        desc: "send job to platform",
+        run: function (env, test) {
+          var job = {
+            platform: 'rss',
+            verb: 'fetch',
+            actor: { address: 'johndoe'},
+            object: {},
+            target: [{ address: 'http://blog.silverbucket.net/rss'}],
+            sessionId: env.sid
+          };
+
+          env.util.redis.set('lpush', env.job_channel, JSON.stringify(job));
+          env.util.redis.get('brpop', env.resp_channel, function (err, resp) {
+            var r = JSON.parse(resp[1]);
+            test.assert(r.status, true);
+          });
+        }
+      },
+      {
+        desc: "shutdown listener",
+        run: function (env, test) {
+          env.listener[0].shutdown().then(function () {
+            test.result(true);
+          }, function (err) {
+            test.result(false, err);
+          });
+        }
+      }
+
+    ]
+  });
+
+  return suites;
+});
