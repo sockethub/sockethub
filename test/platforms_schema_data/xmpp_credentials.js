65f985ccb615940e8a54f03281aeb2ca7c0c2a1e
Nick Jennings <nick@silverbucket.net>
2013-08-21 18:55:07 +0200

    schema tests for xmpp platform credentials. #107

---
+++ test/platforms_schema_data/xmpp_credentials.js
@@ -0,0 +1,56 @@
+var cfg = {
+  'platform': 'xmpp',
+  'type': 'set',
+  'base_prop': 'credentials',
+  'tests': []
+};
+
+
+// basic
+cfg.tests.push({
+  'willFail': false,
+  'data': {
+    'credentials': { // still need to figure a way to define this as invalid
+    }
+  }
+});
+
+cfg.tests.push({
+  'willFail': true,
+  'data': {
+    'sdfsdf': 'asdasd'
+  }
+});
+
+
+// creds
+cfg.tests.push({
+  'willFail': true,
+  'data': {
+    'credentials': {
+      'server': 'asdasdasdasd',
+      'username': 'asdasdasdasd',
+      'password': 'asdasdasdasd',
+      'resource': 'asdasdasdasd',
+      'port': 123
+    }
+  }
+});
+
+cfg.tests.push({
+  'willFail': false,
+  'data': {
+    'credentials': {
+      'bob' : {
+        'server': 'asdasdasdasd',
+        'username': 'asdasdasdasd',
+        'password': 'asdasdasdasd',
+        'resource': 'asdasdasdasd',
+        'port': 123
+      }
+    }
+  }
+});
+
+
+module.exports = cfg;
