6c4785ddaa7174825360cc2d175067ea1fc53f93
Nick Jennings <nick@silverbucket.net>
2013-08-21 18:37:14 +0200

    forgot to add schema data dir. #107

---
+++ test/platforms_schema_data/twitter_credentials.js
@@ -0,0 +1,77 @@
+var cfg = {
+  'platform': 'twitter',
+  'type': 'set',
+  'base_prop': 'credentials',
+  'tests': []
+};
+
+
+// basic
+cfg.tests.push({
+  'willFail': false,
+  'data': {
+    'credentials': { // still need to figure a way to define this as invalid
+    }
+  }
+});
+
+cfg.tests.push({
+  'willFail': true,
+  'data': {
+    'sdfsdf': 'asdasd'
+  }
+});
+
+cfg.tests.push({
+  'willFail': true,
+  'data': {
+    'credentials': {
+      'accessToken': 'asdasdasdasd',
+      'accessTokenSecret': 'asdasdasdasd',
+      'consumerSecret': 'asdasdasdasd',
+      'consumerKey': 'asdasdasdasd'
+    }
+  }
+});
+
+cfg.tests.push({
+  'willFail': true,
+  'data': {
+    'credentials': {
+      'accessed_token' : 'sadfsdfsdf',
+      'access_token_secret' : 'asdasdas',
+      'consumer_secret' : 'sdfsfsdf',
+      'consumer_key' : 'sdfsfsdf'
+    }
+  }
+});
+
+cfg.tests.push({
+  'willFail': true,
+  'data': {
+    'credentials': {
+      'yoyo' : {
+        'accessed_token' : 'sadfsdfsdf',
+        'access_token_secret' : 'asdasdas',
+        'consumer_secret' : 'sdfsfsdf',
+        'consumer_key' : 'sdfsfsdf'
+      }
+    }
+  }
+});
+
+cfg.tests.push({
+  'willFail': false,
+  'data': {
+    'credentials': {
+      'jdoe': {
+        'access_token' : 'sadfsdfsdf',
+        'access_token_secret' : 'asdasdas',
+        'consumer_secret' : 'sdfsfsdf',
+        'consumer_key' : 'sdfsfsdf'
+      }
+    }
+  }
+});
+
+module.exports = cfg;
