4679e1d0630422e19e5982882ded2ed66515a387
Nick Jennings <nick@silverbucket.net>
2013-09-30 23:36:47 +0200

    more tests for client manager

--- test/08-client-manager.js
+++ test/08-client-manager.js
@@ -11,16 +11,17 @@ define(['require'], function(require) {
     desc: "initialize and test the results of the ClientManager singleton",
     setup: function(env, test) {
       env.platform = 'test';
-      env.sessionId = '12345';
-      env.ClientManager = require('./../lib/sockethub/client-manager')(env.platform, env.sessionId);
-      test.assertType(env.ClientManager.get, 'function');
+      env.cm1 = require('./../lib/sockethub/client-manager')(env.platform, '12345');
+      env.cm2 = require('./../lib/sockethub/client-manager')(env.platform, '54321');
+      env.cm3 = require('./../lib/sockethub/client-manager')(env.platform, '98765');
+      test.assertType(env.cm1.get, 'function');
     },
 
     tests: [
       {
         desc: 'get something that doesnt exist',
         run: function (env, test) {
-          test.assert(env.ClientManager.get('test-client', {}), undefined);
+          test.assert(env.cm1.get('test-client', {}), undefined);
         }
       },
       {
@@ -28,7 +29,7 @@ define(['require'], function(require) {
         willFail: true,
         run: function (env, test) {
           try {
-            env.ClientManager.add('test-client', {
+            env.cm1.add('test-client', {
               end: function () {}
             });
             test.result(true);
@@ -42,7 +43,7 @@ define(['require'], function(require) {
         desc: 'add a client with correct params (no credentials)',
         run: function (env, test) {
           try {
-            env.ClientManager.add('test-client', {
+            env.cm1.add('test-client', {
               end: function () {},
               credentials: {}
             });
@@ -57,7 +58,7 @@ define(['require'], function(require) {
         desc: 'fetch client without creds',
         run: function (env, test) {
           try {
-            var client = env.ClientManager.get('test-client', {});
+            var client = env.cm1.get('test-client', {});
             test.assertType(client, 'object');
           } catch (e) {
             test.result(false, e);
@@ -69,7 +70,7 @@ define(['require'], function(require) {
         desc: 'add a client with credentials',
         run: function (env, test) {
           try {
-            env.ClientManager.add('test-client-2', {
+            env.cm1.add('test-client-2', {
               end: function () {},
               credentials: {
                 pepper: 'salt',
@@ -92,10 +93,10 @@ define(['require'], function(require) {
       },
 
       {
-        desc: 'fetch client with creds',
+        desc: 'fetch client with creds. cm1',
         run: function (env, test) {
           try {
-            var client = env.ClientManager.get('test-client-2', {
+            var client = env.cm1.get('test-client-2', {
               pepper: 'salt',
               ketchup: 'mustard',
               yo: [ 'one', 'two', 'three' ],
@@ -108,6 +109,67 @@ define(['require'], function(require) {
               }
             });
             test.assertType(client, 'object');
+          } catch (e) {
+            test.result(false, e);
+          }
+        }
+      },
+
+      {
+        desc: 'fetch client with creds. cm2',
+        run: function (env, test) {
+          try {
+            var client = env.cm2.get('test-client-2', {
+              pepper: 'salt',
+              ketchup: 'mustard',
+              yo: [ 'one', 'two', 'three' ],
+              deep: {
+                objects: 'that',
+                are: 'deep',
+                and: {
+                  deeper: true
+                }
+              }
+            });
+            test.assertType(client, 'object');
+          } catch (e) {
+            test.result(false, e);
+          }
+        }
+      },
+
+      {
+        desc: 'get count on test-client-2',
+        run: function (env, test) {
+          try {
+            var count = env.cm1.referenceCount('test-client-2');
+            console.log('COUNT: '+count);
+            test.assert(count, 2);
+          } catch (e) {
+            test.result(false, e);
+          }
+        }
+      },
+
+      {
+        desc: 'remove one listener',
+        run: function (env, test) {
+          try {
+            env.cm1.remove('test-client-2');
+            test.result(true);
+          } catch (e) {
+            test.result(false, e);
+          }
+        }
+      },
+
+      {
+        desc: 'get count on test-client-2 again',
+        run: function (env, test) {
+          try {
+            var count = env.cm1.referenceCount('test-client-2');
+            console.log('COUNT: '+count);
+            test.assert(count, 1);
           } catch (e) {
             test.result(false, e);
           }
