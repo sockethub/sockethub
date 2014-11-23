6b61170bfaf6aceb73bd823126551ebec5af8f97
Nick Jennings <nick@silverbucket.net>
2014-11-23 02:31:31 +0100

    remove 0.x files, clean start

--- test/scratch/README.md
+++
@@ -1,3 +0,0 @@
-scratch directory for various experiments
-nothing to see here
-
--- test/scratch/client-manager-test.js
+++
@@ -1,7 +0,0 @@
-var ClientManager = require('./../../lib/sockethub/client-manager.js');
-var cm = ClientManager('test', '123');
-
-
-cm.addClient('testkey', {end: function(){ console.log('hallo, ending');}});
-console.log(cm.getClientReferences('testkey'));
-cm.removeClient('testkey');
