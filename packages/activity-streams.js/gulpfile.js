eb149ad445d123ae3d98a74b6b092c31f68c9db9
Nick Jennings <nick@silverbucket.net>
2014-11-23 22:56:05 +0100

    initial checkin

---
+++ packages/activity-streams.js/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp    = require('gulp');
+var minify  = require('minify');
+var fs      = require('fs');
+var pkg     = require('./package.json');
+
+var credits = "/* activity-streams.js v" + pkg.version + " | (c) 2012-2014 Nick Jennings | License: MIT | https://github.com/silverbucket/activity-streams.js */\n";
+var header = "(function(w,undefined){var scope={};";
+var footer = "w.Activity=scope.Activity;})(window);\n";
+
+gulp.task('default', function () {
+
+  var data = credits + header;
+  minify('lib/activity-streams.js', {
+    returnName  : true,
+    log         : true
+  }, function (error, asData) {
+    if (error) {
+      throw new Error(error);
+    }
+
+    minify('node_modules/array-keys/array-keys.js', {
+      returnName  : true,
+      log         : true
+    }, function (error, akData) {
+      if (error) {
+        throw new Error(error);
+      }
+
+      data += akData + asData + footer;
+      fs.writeFile('build/activity-streams.min.js', data);
+    });
+  });
+
+});
