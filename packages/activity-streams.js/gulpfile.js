35055c5af6a29abc0900e5487bee92a77728affd
Nick Jennings <nick@silverbucket.net>
2014-12-04 11:12:46 +0100

    improved build file

--- packages/activity-streams.js/gulpfile.js
+++ packages/activity-streams.js/gulpfile.js
@@ -1,40 +1,16 @@
-var gulp    = require('gulp');
-var minify  = require('minify');
-var fs      = require('fs');
-var pkg     = require('./package.json');
-
-var credits = "/* activity-streams.js v" + pkg.version + " | (c) 2012-2014 Nick Jennings | License: MIT | https://github.com/silverbucket/activity-streams.js */\n";
-var header = "(function(w,undefined){var scope={};";
-var footer = "w.Activity=scope.Activity;})(window);\n";
+var gulp       = require('gulp'),
+    fs         = require('fs'),
+    browserify = require('gulp-browserify'),
+    uglify     = require('gulp-uglify'),
+    rename     = require('gulp-rename'),
+    pkg        = require('./package.json');
 
 gulp.task('default', function () {
 
-  var data = credits + header;
-  minify('lib/activity-streams.js', {
-    returnName  : true,
-    log         : true
-  }, function (error, asData) {
-    if (error) {
-      throw new Error(error);
-    }
-    minify('node_modules/array-keys/array-keys.js', {
-      returnName  : true,
-      log         : true
-    }, function (error, akData) {
-      if (error) {
-        throw new Error(error);
-      }
-      minify('node_modules/wolfy87-eventemitter/EventEmitter.js', {
-        returnName  : true,
-        log         : true
-      }, function (error, eeData) {
-        if (error) {
-          throw new Error(error);
-        }
-        data += akData + eeData + asData + footer;
-        fs.writeFile('dist/activity-streams.min.js', data);
-      });
-    });
-  });
-
+  gulp.src('lib/activity-streams.js')
+      .pipe(browserify({ standalone: 'Activity' }))
+      .pipe(gulp.dest('dist/'))
+      .pipe(uglify())
+      .pipe(rename('activity-streams.min.js'))
+      .pipe(gulp.dest('dist/'));
 });
