f0b6dcf76886207ab25a8330de065799e529f6c0
Nick Jennings <nick@silverbucket.net>
2014-12-06 20:46:31 +0100

    version bump

--- packages/activity-streams.js/browser/activity-streams.js
+++ packages/activity-streams.js/browser/activity-streams.js
@@ -1,7 +1,7 @@
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Activity=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 /*!
  * activity-streams
- *   version 0.3.0
+ *   version 0.3.1
  *   http://github.com/silverbucket/activity-streams
  *
  * Developed and Maintained by:
@@ -25,27 +25,23 @@ var objs    = new ArrayKeys({ identifier: 'id' }),
     ee      = EventEmitter();
 
 var Stream = function (stream) {
-  var actorObj, targetObj, objectObj;
-  if ((typeof stream.actor === 'string') &&
-      (actorObj = objs.getRecord(stream.actor))) {
-    stream.actor = actorObj;
+
+  if (typeof stream.actor === 'string') {
+    stream.actor = objs.getRecord(stream.actor) || stream.actor;
   }
 
-  if ((typeof stream.target === 'string') &&
-      (actorObj = objs.getRecord(stream.target))) {
-    stream.target = targetObj;
+  if (typeof stream.target === 'string') {
+    stream.target = objs.getRecord(stream.target) || stream.target;
   } else if (Array.isArray(stream.target)) {
     for (var i = stream.target.length - 1; i >= 0; i -= 1) {
-      if ((typeof stream.target[i] === 'string') &&
-          (targetObj = objs.getRecord(stream.target[i]))) {
-        stream.target[i] = targetObj;
+      if (typeof stream.target[i] === 'string') {
+        stream.target[i] = objs.getRecord(stream.target[i]) || stream.target[i];
       }
     }
   }
 
-  if ((typeof stream.object === 'string') &&
-      (objectObj = objs.getRecord(stream.object))) {
-    stream.object = objectObj;
+  if (typeof stream.object === 'string') {
+    stream.object = objs.getRecord(stream.object) || stream.object;
   }
 
   ee.emit('activity-stream', stream);
