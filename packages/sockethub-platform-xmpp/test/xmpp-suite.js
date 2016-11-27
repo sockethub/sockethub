5a831580536ae20b2e611d70bc9b394218027d6d
Nick Jennings <nick@silverbucket.net>
2016-11-27 01:46:28 -0800

    passing tests, cover basic initialization, message sending and new accounts

--- packages/sockethub-platform-xmpp/test/xmpp-suite.js
+++ packages/sockethub-platform-xmpp/test/xmpp-suite.js
@@ -17,11 +17,9 @@ define(['require'], function (require) {
       env.tv4 = require('./../node_modules/tv4/tv4');
 
       // irc-factory mock
-      env.SimpleXMPP = require('./mock-simple-xmpp')(test);
-      env.SimpleXMPP.mock = true;
-      global.SimpleXMPP = env.SimpleXMPP;
-      global.xmpp = env.SimpleXMPP.xmpp();
-      env.xmpp = env.SimpleXMPP.xmpp();
+      env.xmpp = require('./mock-simple-xmpp')(test);
+      env.xmpp.mock = true;
+      global.xmpp = env.xmpp;
 
       var Platform = require('./../index');
       env.platform = new Platform(env.session);
@@ -32,10 +30,10 @@ define(['require'], function (require) {
         displayName:'testingham'
       };
 
-      env.newActor = {
+      env.actor2 = {
         '@type': 'person',
-        '@id': 'xmpp://testler@jabber.net',
-        displayName:'testler'
+        '@id': 'xmpp://testingturkey@jabber.net',
+        displayName:'testingturkey'
       };
 
       env.credentials = {
@@ -43,6 +41,17 @@ define(['require'], function (require) {
         object: {
           '@type': 'credentials',
           username: 'testingham',
+          server: 'jabber.net',
+          password: 'foobar',
+          resource: 'home'
+        }
+      };
+
+      env.credentials2 = {
+        actor: env.actor2,
+        object: {
+          '@type': 'credentials',
+          username: 'testingturkey',
           server: 'jabber.net',
           password: 'foobar',
           resource: 'home'
@@ -73,16 +82,6 @@ define(['require'], function (require) {
       };
 
       env.job = {
-        // join: {
-        //   sockethub: {
-        //     actor: env.actor,
-        //     target: env.target.sockethub
-        //   },
-        //   remotestorage: {
-        //     actor: env.actor,
-        //     target: env.target.remotestorage
-        //   }
-        // },
         send: {
           actor: env.actor,
           object: {
@@ -91,26 +90,7 @@ define(['require'], function (require) {
           },
           target: env.target.mrfoobar
         },
-        // observe: {
-        //   actor: env.actor,
-        //   object: {
-        //     '@type': 'attendance'
-        //   },
-        //   target: env.target.mrfoobar
-        // },
         update: {
-          // topic: {
-          //   actor: env.actor,
-          //   object: {
-          //     '@type': 'topic',
-          //     topic: 'welcome to unit testing, enjoy your stay - the management'
-          //   },
-          //   target: env.target.sockethub
-          // },
-          // nick: {
-          //   actor: env.actor,
-          //   target: env.newActor
-          // }
           presence: {
             actor: env.actor,
             object: {
@@ -119,6 +99,17 @@ define(['require'], function (require) {
               status: 'available'
             }
           }
+        }
+      };
+
+      env.job2 = {
+        send: {
+          actor: env.actor2,
+          object: {
+            '@type': 'message',
+            content: 'hello'
+          },
+          target: env.target.mrfoobar
         }
       };
 
@@ -169,200 +160,63 @@ define(['require'], function (require) {
         }
       },
 
-      // {
-      //   desc: "# join 1 channel",
-      //   run: function (env, test) {
-      //     env.platform.join(env.job.join.sockethub, function (err, result) {
-      //       test.assertTypeAnd(err, 'undefined', err);
-      //       test.assertType(result, 'undefined');
-      //     });
-      //   }
-      // },
-      // {
-      //   desc: "# join 1 - check stubs",
-      //   run: function (env, test) {
-      //     test.assertAnd(env.xmpp.connect.numCalled, 1);
-      //     test.assert(env.SimpleXMPP.ClientNumCalled(0), 1); // #sockethub
-      //   }
-      // },
-
-      // {
-      //   desc: "# join 2nd channel",
-      //   run: function (env, test) {
-      //     env.platform.join(env.job.join.remotestorage, function (err, result) {
-      //       test.assertTypeAnd(err, 'undefined', err);
-      //       test.assertType(result, 'undefined');
-      //     });
-      //   }
-      // },
-      // {
-      //   desc: "# join 2 - check stubs",
-      //   run: function (env, test) {
-      //     test.assertAnd(env.api.connect.numCalled, 1);
-      //     test.assert(env.SimpleXMPP.ClientNumCalled(0), 2); // #remotestorage
-      //   }
-      // },
-
       {
-        desc: "# send",
+        desc: "# send 1",
         run: function (env, test) {
           env.platform.send(env.job.send, function (err, result) {
             test.assertTypeAnd(err, 'undefined', err);
-            test.assertType(result, 'undefined');
+            test.assertTypeAnd(result, 'undefined');
+            test.assert(env.xmpp.send.numCalled, 1);
           });
         }
       },
       {
-        desc: "# send",
+        desc: "# send 2",
         run: function (env, test) {
           env.platform.send(env.job.send, function (err, result) {
             test.assertTypeAnd(err, 'undefined', err);
-            test.assertType(result, 'undefined');
+            test.assertTypeAnd(result, 'undefined');
+            test.assert(env.xmpp.send.numCalled, 2);
           });
         }
       },
       {
         desc: "# send - check stubs",
         run: function (env, test) {
-          var verb = 'send';
-          test.assertAnd(env.xmpp.send.numCalled, 2);
-          test.assert(env.SimpleXMPP.ClientNumCalled(0), 0);
+          test.assert(env.xmpp.connect.numCalled, 1);
         }
       },
-
-      // {
-      //   desc: "# leave",
-      //   run: function (env, test) {
-      //     env.platform.leave(env.job.leave, function (err, result) {
-      //       test.assertTypeAnd(err, 'undefined', err);
-      //       test.assertType(result, 'undefined');
-      //     });
-      //   }
-      // },
-      // {
-      //   desc: "# leave - check stubs",
-      //   run: function (env, test) {
-      //     test.assertAnd(env.api.connect.numCalled, 1);
-      //     test.assert(env.SimpleXMPP.ClientNumCalled(0), 4); // #remotestorage
-      //   }
-      // },
-      // {
-      //   desc: "# check internal prop _channels",
-      //   run: function (env, test) {
-      //     test.assert(env.platform._channels, ['#sockethub']);
-      //   }
-      // },
-
-      // {
-      //   desc: "# observe",
-      //   run: function (env, test) {
-      //     env.platform.observe(env.job.observe, function (err, result) {
-      //       test.assertTypeAnd(err, 'undefined', err);
-      //       test.assertType(result, 'undefined');
-      //     });
-      //   }
-      // },
-      // {
-      //   desc: "# observe - check stubs",
-      //   run: function (env, test) {
-      //     test.assertAnd(env.api.connect.numCalled, 1);
-      //     test.assert(env.SimpleXMPP.ClientNumCalled(0), 5); // #sockethub
-      //   }
-      // },
-
-      // {
-      //   desc: "# update topic",
-      //   run: function (env, test) {
-      //     env.platform.update(env.job.update.topic, function (err, result) {
-      //       test.assertTypeAnd(err, 'undefined', err);
-      //       test.assertType(result, 'undefined');
-      //     });
-      //   }
-      // },
-      // {
-      //   desc: "# update topic - check stubs",
-      //   run: function (env, test) {
-      //     test.assertAnd(env.api.connect.numCalled, 1);
-      //     test.assert(env.SimpleXMPP.ClientNumCalled(0), 6); // #sockethub
-      //   }
-      // },
-
-      // {
-      //   desc: "# update nick (rename)",
-      //   run: function (env, test) {
-      //     var called = 0;
-      //     env.platform.update(env.job.update.nick, function (err, result) {
-      //       called += 1;
-      //       test.assertTypeAnd(err, 'undefined', err, err);
-      //       test.assertTypeAnd(result, 'undefined');
-      //       setTimeout(function () {
-      //         test.assert(called, 1);
-      //       }, 1000);
-      //     });
-      //   }
-      // },
-      // {
-      //   desc: "# update nick - check stubs",
-      //   run: function (env, test) {
-      //     test.assertAnd(env.api.connect.numCalled, 1);
-      //     test.assert(env.SimpleXMPP.ClientNumCalled(0), 7);
-      //   }
-      // },
 
       {
         desc: "# update presence",
         run: function (env, test) {
           env.platform.update(env.job.update.presence, function (err, result) {
             test.assertTypeAnd(err, 'undefined', err);
-            test.assertType(result, 'undefined');
+            test.assertTypeAnd(result, 'undefined');
+            test.assert(env.xmpp.setPresence.numCalled, 1);
           });
-        }
-      },
-      {
-        desc: "# update topic - check stubs",
-        run: function (env, test) {
-          test.assertAnd(env.xmpp.setPresence.numCalled, 1);
-          test.assert(env.SimpleXMPP.ClientNumCalled(0), 6); // #sockethub
         }
       },
 
       {
-        desc: "# send with old credentials",
+        desc: "set second credentials",
         run: function (env, test) {
-          env.platform.senf(env.job.send.remotestorage, function (err, result) {
+          env.session.store.save(env.actor2['@id'],
+                                 env.credentials2, function () {
+                                 test.done();
+                                });
+        }
+      },
+
+      {
+        desc: "# send with second credentials",
+        run: function (env, test) {
+          env.platform.send(env.job2.send, function (err, result) {
             test.assertTypeAnd(err, 'undefined', err);
-            test.assertType(result, 'undefined');
+            test.assertTypeAnd(result, 'undefined');
+            test.assertAnd(env.xmpp.send.numCalled, 3);
+            test.assert(env.xmpp.connect.numCalled, 2);
           });
-        }
-      },
-      {
-        desc: "# send with old credentials makes new client (calls createClient again)",
-        run: function (env, test) {
-          test.assertAnd(env.api.connect.numCalled, 2);
-          test.assert(env.SimpleXMPP.ClientNumCalled(1), 1); // #sockethub
-        }
-      },
-
-      {
-        desc: "# send with renamed creds",
-        run: function (env, test) {
-          var job = env.job.send;
-          job.actor = env.newActor;
-
-          env.platform.send(job, function (err, result) {
-            test.assertTypeAnd(err, 'undefined', err);
-            test.assertType(result, 'undefined');
-          });
-        }
-      },
-
-      {
-        desc: "# send with renamed creds - check stubs",
-        run: function (env, test) {
-          // ensure new client was not created
-          test.assertAnd(env.api.connect.numCalled, 2);
-          // new object was not used to send message
-          test.assert(env.SimpleXMPP.ClientNumCalled(0), 8);
         }
       }
     ]
