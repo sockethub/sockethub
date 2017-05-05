7974fecbe1a29feba601517efae29eff8e4dac14
Nick Jennings <nick@silverbucket.net>
2017-05-06 01:34:25 +0200

    build

--- packages/activity-streams.js/browser/activity-streams.js
+++ packages/activity-streams.js/browser/activity-streams.js
@@ -1,7 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ActivityStreams = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
  * activity-streams
- *   version 2.0.5
  *   http://github.com/silverbucket/activity-streams
  *
  * Developed and Maintained by:
@@ -219,6 +218,7 @@ module.exports = function (opts) {
     }
   };
 };
+
 },{"array-keys":2,"event-emitter":17}],2:[function(require,module,exports){
 /*!
  * array-keys
