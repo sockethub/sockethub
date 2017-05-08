0121bac5a2dae126673138fb0c211135518af333
Nick Jennings <nick@silverbucket.net>
2017-05-08 12:45:04 +0200

    move connection manager object to bottom of code

--- packages/sockethub-platform-xmpp/index.js
+++ packages/sockethub-platform-xmpp/index.js
@@ -152,250 +152,6 @@ function jidStripResource(jid) {
   return jid.split('/')[0];
 }
 
-var createObj = {
-  timeout: 30000,
-  connect: function (cb) {
-    var self = this;
-    self.scope.debug('calling connect for ' + self.credentials.actor['@id']);
-
-    //
-    // generate bareJid and fullJid
-    var fullJid;
-    if (self.credentials.object.username.indexOf('@') === -1) {
-      fullJid = self.credentials.object.username + '@' + self.credentials.object.server + '/' + self.credentials.object.resource;
-    } else {
-      fullJid = self.credentials.object.username + '/' + self.credentials.object.resource;
-    }
-
-    //
-    // credential object to pass to simple-xmpp
-    var xmpp_creds = {
-      jid: fullJid,
-      password: self.credentials.object.password
-    };
-    if (self.credentials.object.server) {
-      xmpp_creds.host = self.credentials.object.server;
-    }
-    if (self.credentials.port) {
-      xmpp_creds.port = self.credentials.object.port;
-    }
-
-    var handlers = {
-      error: function (error) {
-        var msg = 'failed connecting ' + fullJid;
-        msg = (error) ? msg + ' : ' + error : msg;
-        self.scope.debug("connect error: " + error);
-        xmpp.disconnect();
-        cb(msg);
-      },
-      online: function () {
-        self.scope.debug('connected with jid: ' + fullJid);
-        xmpp.removeListener('online', handlers.online);
-        xmpp.removeListener('error', handlers.error);
-        xmpp.removeListener('close', handlers.close);
-        cb(null, xmpp);
-      },
-      close: function () {
-        // FIXME - not sure in what cases this happens
-        self.scope.debug('close received for ' + fullJid);
-        cb('received close event for '+ fullJid);
-      }
-    };
-
-    xmpp.on('online', handlers.online);
-    xmpp.on('error', handlers.error);
-    xmpp.on('close', handlers.close);
-
-    xmpp.connect(xmpp_creds);
-    self.scope.debug('sent XMPP connect for account ' + fullJid);
-  },
-  listeners: {
-    stanza: function (stanza) {
-      this.scope.debug("got XMPP stanza... " + stanza);
-
-      // simple-xmpp currently doesn't seem to handle error state presence
-      // so we'll do it here for now.
-      // TODO: consider moving this to simple-xmpp once it's ironed out and
-      // proven to work well.
-      if (stanza.is('presence') && (stanza.type === 'error')) {
-        var error,
-            message = stanza.toString(),
-            type = 'update';
-
-        if (error = stanza.getChild('error')) {
-          message = error.toString();
-          if (error.getChild('remote-server-not-found')) {
-            // when we get this type of return message, we know it was a response from a join
-            type = 'join';
-            message = 'remote server not found ' + stanza.from;
-          }
-        }
-
-        this.scope.send({
-          '@type': type,
-          actor: {
-            '@id': stanza.from,
-            '@type': 'room'
-          },
-          object: {
-            '@type': 'error', // type error
-            content: message.toString()
-          },
-          target: {
-            '@id': stanza.to,
-            '@type': 'person'
-          }
-        });
-      } else if (stanza.is('iq')) {
-        var query = stanza.getChild('query');
-        if (query) {
-          var entries = query.getChildren('item');
-          for (var e in entries) {
-            if (!entries.hasOwnProperty(e)) {
-              continue;
-            }
-            this.scope.debug('STANZA ATTRS: ', entries[e].attrs);
-            if (entries[e].attrs.subscription === 'both') {
-              this.scope.send({
-                '@type': 'update',
-                actor: { '@id': entries[e].attrs.jid, displayName: entries[e].attrs.name },
-                target: this.credentials.actor,
-                object: {
-                  '@type': 'presence',
-                  status: '',
-                  presence: state
-                }
-              });
-            } else if ((entries[e].attrs.subscription === 'from') &&
-                      (entries[e].attrs.ask) && (entries[e].attrs.ask === 'subscribe')) {
-              this.scope.send({
-                '@type': 'update',
-                actor: { '@id': entries[e].attrs.jid, displayName: entries[e].attrs.name },
-                target: this.credentials.actor,
-                object: {
-                  '@type': 'presence',
-                  statusText: '',
-                  presence: 'notauthorized'
-                }
-              });
-            } else {
-              /**
-               * cant figure out how to know if one of these query stanzas are from
-               * added contacts or pending requests
-               */
-              this.scope.send({
-                '@type': 'request-friend',
-                actor: { '@id': entries[e].attrs.jid, displayName: entries[e].attrs.name },
-                target: this.credentials.actor
-              });
-            }
-          }
-        }
-      }
-    },
-    chatstate: function (from, name) {
-      this.scope.debug('received chatstate event: ' + from, name);
-    },
-    groupbuddy: function (id, groupBuddy, state, statusText) {
-      this.scope.debug('received groupbuddy event: ' + id, groupBuddy, state, statusText);
-    },
-    buddyCapabilities: function (id, capabilities) {
-      this.scope.debug('received buddyCapabilities: ' + id, capabilities);
-    },
-    chat: function (from, message) {
-      this.scope.debug("received chat message from " + from);
-      this.scope.send({
-        '@type': 'send',
-        actor: { '@id': from },
-        target: this.credentials.actor,
-        object: {
-          content: message,
-          id: nextId()
-        }
-      });
-    },
-    buddy: function (from, state, statusText) {
-      if (from !== this.credentials.actor['@id']) {
-        this.scope.debug('received buddy presence update: ' + from + ' - ' + state);
-        this.scope.send({
-          '@type': 'update',
-          actor: { '@id': from },
-          target: this.credentials.actor,
-          object: {
-            '@type': 'presence',
-            status: statusText,
-            presence: state
-          }
-        });
-      }
-    },
-    subscribe: function (from) {
-      this.scope.debug('received subscribe request from ' + from);
-      this.scope.send({
-        '@type': "request-friend",
-        actor: { '@id': from },
-        target: this.credentials.actor
-      });
-    },
-    unsubscribe: function (from) {
-      this.scope.debug('received unsubscribe request from ' + from);
-      this.scope.send({
-        '@type': "remove-friend",
-        actor: { '@id': from },
-        target: this.credentials.actor
-      });
-    },
-    close: function () {
-      this.scope.debug('received close event with no handler specified');
-      this.scope.send({
-        '@type': 'close',
-        actor: this.credentials.actor,
-        target: this.credentials.actor
-      });
-      this.scope.debug('**** xmpp session for ' + this.credentials.actor['@id'] + ' closed');
-      this.connection.disconnect();
-    },
-    error: function (error) {
-      try {
-        this.scope.debug("*** XMPP ERROR (rl): " + error);
-        this.scope.send({
-          '@type': 'error',
-          object: {
-            '@type': 'error',
-            content: error
-          }
-        });
-      } catch (e) {
-        this.scope.debug('*** XMPP ERROR (rl catch): ', e);
-      }
-    },
-    online: function () {
-      this.scope.debug('online');
-      this.scope.debug('reconnectioned ' + this.credentials.actor['@id']);
-    }
-  },
-  addListener: function (name, func) {
-    this.connection.on(name, func);
-  },
-  removeListener: function (name) {
-    this.connection.removeListener(name);
-  },
-  isConnected: function () {
-    if (this.connection.STATUS === 'offline') {
-      return false;
-    } else {
-      return true;
-    }
-  },
-  disconnect: function (cb) {
-    // FIXME - review this, simple-xmpp has a close func now i believe
-    this.scope.debug('should be CLOSING connection now, NOT IMPLEMENTED in node-xmpp');
-    this.scope.quit = true;
-    this.connection.disconnect();
-    cb();
-  }
-};
-
 /**
  * Function: connect
  *
@@ -648,6 +404,254 @@ XMPP.prototype.cleanup = function (done) {
   // FIXME
   this.session.debug('cleanup called, but nothing implemented');
   done();
+};
+
+
+//
+// connection manager object for establishing XMPP connection (connect property), and handling incoming events (listeners property), and more.
+//
+var createObj = {
+  timeout: 30000,
+  connect: function (cb) {
+    var self = this;
+    self.scope.debug('calling connect for ' + self.credentials.actor['@id']);
+
+    //
+    // generate bareJid and fullJid
+    var fullJid;
+    if (self.credentials.object.username.indexOf('@') === -1) {
+      fullJid = self.credentials.object.username + '@' + self.credentials.object.server + '/' + self.credentials.object.resource;
+    } else {
+      fullJid = self.credentials.object.username + '/' + self.credentials.object.resource;
+    }
+
+    //
+    // credential object to pass to simple-xmpp
+    var xmpp_creds = {
+      jid: fullJid,
+      password: self.credentials.object.password
+    };
+    if (self.credentials.object.server) {
+      xmpp_creds.host = self.credentials.object.server;
+    }
+    if (self.credentials.port) {
+      xmpp_creds.port = self.credentials.object.port;
+    }
+
+    var handlers = {
+      error: function (error) {
+        var msg = 'failed connecting ' + fullJid;
+        msg = (error) ? msg + ' : ' + error : msg;
+        self.scope.debug("connect error: " + error);
+        xmpp.disconnect();
+        cb(msg);
+      },
+      online: function () {
+        self.scope.debug('connected with jid: ' + fullJid);
+        xmpp.removeListener('online', handlers.online);
+        xmpp.removeListener('error', handlers.error);
+        xmpp.removeListener('close', handlers.close);
+        cb(null, xmpp);
+      },
+      close: function () {
+        // FIXME - not sure in what cases this happens
+        self.scope.debug('close received for ' + fullJid);
+        cb('received close event for '+ fullJid);
+      }
+    };
+
+    xmpp.on('online', handlers.online);
+    xmpp.on('error', handlers.error);
+    xmpp.on('close', handlers.close);
+
+    xmpp.connect(xmpp_creds);
+    self.scope.debug('sent XMPP connect for account ' + fullJid);
+  },
+  listeners: {
+    stanza: function (stanza) {
+      this.scope.debug("got XMPP stanza... " + stanza);
+
+      // simple-xmpp currently doesn't seem to handle error state presence
+      // so we'll do it here for now.
+      // TODO: consider moving this to simple-xmpp once it's ironed out and
+      // proven to work well.
+      if (stanza.is('presence') && (stanza.type === 'error')) {
+        var error,
+            message = stanza.toString(),
+            type = 'update';
+
+        if (error = stanza.getChild('error')) {
+          message = error.toString();
+          if (error.getChild('remote-server-not-found')) {
+            // when we get this type of return message, we know it was a response from a join
+            type = 'join';
+            message = 'remote server not found ' + stanza.from;
+          }
+        }
+
+        this.scope.send({
+          '@type': type,
+          actor: {
+            '@id': stanza.from,
+            '@type': 'room'
+          },
+          object: {
+            '@type': 'error', // type error
+            content: message.toString()
+          },
+          target: {
+            '@id': stanza.to,
+            '@type': 'person'
+          }
+        });
+      } else if (stanza.is('iq')) {
+        var query = stanza.getChild('query');
+        if (query) {
+          var entries = query.getChildren('item');
+          for (var e in entries) {
+            if (!entries.hasOwnProperty(e)) {
+              continue;
+            }
+            this.scope.debug('STANZA ATTRS: ', entries[e].attrs);
+            if (entries[e].attrs.subscription === 'both') {
+              this.scope.send({
+                '@type': 'update',
+                actor: { '@id': entries[e].attrs.jid, displayName: entries[e].attrs.name },
+                target: this.credentials.actor,
+                object: {
+                  '@type': 'presence',
+                  status: '',
+                  presence: state
+                }
+              });
+            } else if ((entries[e].attrs.subscription === 'from') &&
+                      (entries[e].attrs.ask) && (entries[e].attrs.ask === 'subscribe')) {
+              this.scope.send({
+                '@type': 'update',
+                actor: { '@id': entries[e].attrs.jid, displayName: entries[e].attrs.name },
+                target: this.credentials.actor,
+                object: {
+                  '@type': 'presence',
+                  statusText: '',
+                  presence: 'notauthorized'
+                }
+              });
+            } else {
+              /**
+               * cant figure out how to know if one of these query stanzas are from
+               * added contacts or pending requests
+               */
+              this.scope.send({
+                '@type': 'request-friend',
+                actor: { '@id': entries[e].attrs.jid, displayName: entries[e].attrs.name },
+                target: this.credentials.actor
+              });
+            }
+          }
+        }
+      }
+    },
+    chatstate: function (from, name) {
+      this.scope.debug('received chatstate event: ' + from, name);
+    },
+    groupbuddy: function (id, groupBuddy, state, statusText) {
+      this.scope.debug('received groupbuddy event: ' + id, groupBuddy, state, statusText);
+    },
+    buddyCapabilities: function (id, capabilities) {
+      this.scope.debug('received buddyCapabilities: ' + id, capabilities);
+    },
+    chat: function (from, message) {
+      this.scope.debug("received chat message from " + from);
+      this.scope.send({
+        '@type': 'send',
+        actor: { '@id': from },
+        target: this.credentials.actor,
+        object: {
+          content: message,
+          id: nextId()
+        }
+      });
+    },
+    buddy: function (from, state, statusText) {
+      if (from !== this.credentials.actor['@id']) {
+        this.scope.debug('received buddy presence update: ' + from + ' - ' + state);
+        this.scope.send({
+          '@type': 'update',
+          actor: { '@id': from },
+          target: this.credentials.actor,
+          object: {
+            '@type': 'presence',
+            status: statusText,
+            presence: state
+          }
+        });
+      }
+    },
+    subscribe: function (from) {
+      this.scope.debug('received subscribe request from ' + from);
+      this.scope.send({
+        '@type': "request-friend",
+        actor: { '@id': from },
+        target: this.credentials.actor
+      });
+    },
+    unsubscribe: function (from) {
+      this.scope.debug('received unsubscribe request from ' + from);
+      this.scope.send({
+        '@type': "remove-friend",
+        actor: { '@id': from },
+        target: this.credentials.actor
+      });
+    },
+    close: function () {
+      this.scope.debug('received close event with no handler specified');
+      this.scope.send({
+        '@type': 'close',
+        actor: this.credentials.actor,
+        target: this.credentials.actor
+      });
+      this.scope.debug('**** xmpp session for ' + this.credentials.actor['@id'] + ' closed');
+      this.connection.disconnect();
+    },
+    error: function (error) {
+      try {
+        this.scope.debug("*** XMPP ERROR (rl): " + error);
+        this.scope.send({
+          '@type': 'error',
+          object: {
+            '@type': 'error',
+            content: error
+          }
+        });
+      } catch (e) {
+        this.scope.debug('*** XMPP ERROR (rl catch): ', e);
+      }
+    },
+    online: function () {
+      this.scope.debug('online');
+      this.scope.debug('reconnectioned ' + this.credentials.actor['@id']);
+    }
+  },
+  addListener: function (name, func) {
+    this.connection.on(name, func);
+  },
+  removeListener: function (name) {
+    this.connection.removeListener(name);
+  },
+  isConnected: function () {
+    if (this.connection.STATUS === 'offline') {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  disconnect: function (cb) {
+    // FIXME - review this, simple-xmpp has a close func now i believe
+    this.scope.debug('should be CLOSING connection now, NOT IMPLEMENTED in node-xmpp');
+    this.scope.quit = true;
+    this.connection.disconnect();
+    cb();
+  }
 };
 
 module.exports = XMPP;
