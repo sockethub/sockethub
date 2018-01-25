109d30831237952f59df99533903944ddcdd7942
Nick Jennings <nick@silverbucket.net>
2018-01-25 18:37:08 +0000

    initial working version of irc2as

---
+++ packages/irc2as/index.js
@@ -0,0 +1,326 @@
+const events = require('events');
+
+const ERR_CHAN_PRIVS = "482",
+      ERR_NICK_IN_USE = "433",
+      ERR_NO_CHANNEL = "403",
+      JOIN = "JOIN",
+      MOTD = "372",
+      MOTD_END = "376",
+      NAMES = "353",
+      NAMES_END = "366",
+      NICK = "NICK",
+      PART = "PART",
+      PRIVMSG = "PRIVMSG",
+      QUIT = "QUIT",
+      TOPIC_CHANGE = "TOPIC",
+      TOPIC_IS = "332",
+      TOPIC_SET_BY = "333";
+
+function IrcToActivityStreams(cfg) {
+    const config = cfg || {};
+    this.server = config.server;
+    this.events = new events.EventEmitter();
+    this.__buffer = {};
+    this.__buffer[NAMES] = {};
+    return this;
+}
+
+IrcToActivityStreams.prototype.input = function (string) {
+    if (typeof string !== 'string') { return false; }
+    if (string.length < 5) { return false; }
+    string = string.trim();
+    const time = Date.now();
+    const [metadata, content] = string.split(' :');
+    const [server, code, pos1, pos2, pos3, ...msg] = metadata.split(" ");
+    const channel = ((typeof pos1 === "string") && (pos1.startsWith('#'))) ? pos1 : 
+                    ((typeof pos2 === "string") && (pos2.startsWith('#'))) ? pos2 : 
+                    ((typeof pos3 === "string") && (pos3.startsWith('#'))) ? pos3 : undefined;
+    let nick, type, message;
+
+    switch (code) {
+        /** */
+        case ERR_CHAN_PRIVS: // privs not sufficient
+        this.events.emit('stream', {
+            '@type': 'send',
+            actor: {
+              '@type': 'room',
+              '@id': 'irc://' + this.server + '/' + channel
+            },
+            target: {
+              '@type': 'person',
+              '@id': 'irc://' + pos1 + '@' + this.server
+            },
+            object: {
+              '@type': 'message',
+              content: content
+            }
+        });
+        break;
+
+        /** */
+        case ERR_NICK_IN_USE: // nick conflict
+        this.events.emit('stream', {
+            '@type': 'update',
+            actor: {
+                '@type': 'person',
+                '@id': 'irc://' + pos1 + '@' + this.server,
+                displayName: pos1
+            },
+            object: {
+                '@type': 'error',
+                content: content
+            },
+            target: {
+                '@type': 'person',
+                '@id': 'irc://' + pos2 + '@' + this.server,
+                displayName: pos2
+            },
+            published: time
+        })
+        break;
+
+        /** */
+        case ERR_NO_CHANNEL: // no such channel
+        this.events.emit('stream', {
+            '@type': 'join',
+            actor: {
+                '@id': 'irc://' + this.server,
+                '@type': 'service'
+            },
+            object: {
+                '@type': 'error',
+                content: 'no such channel ' + pos2
+            },
+            target: {
+                '@id': 'irc://' + pos2 + '@' + this.server,
+                '@type': 'person'
+            },
+            published: time
+        });
+        break;
+    
+        /** */
+        case JOIN: // room join
+        nick = server.split(/^:/)[1].split('!')[0];
+        this.events.emit('stream', {
+            '@type': 'join',
+            actor: {
+                '@type': 'person',
+                '@id': 'irc://' + nick + '@' + this.server,
+                displayName: nick
+            },
+            target: {
+                '@type': 'room',
+                '@id': 'irc://' + this.server + '/' + channel,
+                displayName: channel
+            },
+            object: {},
+            published: time
+        });
+        break;
+
+        /** */
+        case MOTD: // MOTD
+        if (! this.__buffer[MOTD]) {
+            this.__buffer[MOTD] = {
+                '@type': 'update',
+                actor: {
+                    '@type': 'service',
+                    '@id': 'irc://' + this.server,
+                    displayName: this.server
+                },
+                object: {
+                    '@type': 'topic',
+                    content: [ content ]
+                },
+                published: time
+            }
+        } else {
+            this.__buffer[MOTD].object.content.push(content);
+        }
+        break; 
+        case MOTD_END: // end of MOTD
+        this.events.emit('stream', this.__buffer[MOTD]);
+        delete this.__buffer[MOTD];
+        break;
+
+        /** */
+        case NAMES:  // user list
+        if (! this.__buffer[NAMES][channel]) {
+            this.__buffer[NAMES][channel] = {
+                '@type': 'observe',
+                actor: {
+                    '@type': 'room',
+                    '@id': 'irc://' + this.server + '/' + channel,
+                    displayName: channel
+                },
+                object: {
+                    '@type': 'attendance',
+                    members: content.split(' ')
+                },
+                published: time
+            };
+        } else {
+            this.__buffer[NAMES][channel].object.members.push(content.split(' '));
+        }
+        break;
+        case NAMES_END: // end user list
+        this.events.emit('stream', this.__buffer[NAMES][channel]);
+        delete this.__buffer[NAMES][channel];
+        break;
+
+        /** */
+        case NICK: // nick change
+        nick = server.split(/^:/)[1].split('!')[0];
+        this.events.emit('stream', {
+            '@type': 'update',
+            actor: {
+                '@type': 'person',
+                '@id': 'irc://' + nick + '@' + this.server,
+                displayName: nick
+            },
+            target: {
+                '@type': 'person',
+                '@id': 'irc://' + content + '@' + this.server,
+                displayName: content
+            },
+            object: {
+                '@type': 'address'
+            },
+            published: time
+        });
+        break;
+
+        /** */
+        case PART: // leaving
+        nick = server.split(/^:/)[1].split('!')[0];
+        this.events.emit('stream', {
+            '@type': 'leave',
+            actor: {
+                '@type': 'person',
+                '@id': 'irc://' + nick + '@' + this.server, 
+                displayName: nick
+            },
+            target: {
+                '@type': 'room',
+                '@id': 'irc://' + this.server + '/' + channel, 
+                displayName: channel
+            },
+            object: {
+                '@type': 'message',
+                content: 'user has left the channel'
+            },
+            published: time
+        });
+        break;
+
+        /** */
+        case PRIVMSG: // msg
+        nick = server.split(/^:/)[1].split('!')[0];
+        if (content.startsWith('+\u0001ACTION ')) {
+            type = 'me';
+            message = content.split(/^\+\u0001ACTION\s+/)[1].split(/\u0001$/)[0];
+        } else {
+            type = 'message';
+            message = content;
+        }
+
+        this.events.emit('stream', {
+            '@type': 'send',
+            actor: {
+              '@type': 'person',
+              '@id': 'irc://' + nick + '@' + this.server,
+              displayName: nick
+            },
+            target: {
+              displayName: pos1
+            },
+            object: {
+              '@type': type,
+              content: message
+            },
+            published: time
+        });
+        break;
+
+        /** */
+        case QUIT: // quit user
+        nick = server.split(/^:/)[1].split('!')[0];
+        this.events.emit('stream', {
+            '@type': 'leave',
+            actor: {
+                '@type': 'person',
+                '@id': 'irc://' + nick + '@' + this.server, 
+                displayName: nick
+            },
+            target: {
+                '@type': 'service',
+                '@id': 'irc://' + this.server 
+            },
+            object: {
+                '@type': 'message',
+                content: 'user has quit'
+            },
+            published: time
+        });
+        break;
+
+        /** */
+        case TOPIC_CHANGE: // topic changed now
+        nick = server.split(/^:/)[1].split('!')[0];
+        this.events.emit('stream', {
+            '@type': 'update',
+            actor: {
+                '@type': 'person',
+                '@id': 'irc://' + nick + '@' + this.server,
+                displayName: nick
+            },
+            target: {
+                '@type': 'room',
+                '@id': 'irc://' + this.server + '/' + channel,
+                displayName: channel
+            },
+            object: {
+                '@type': 'topic',
+                topic: content
+            },
+            published: time
+        });
+        break;
+
+        /** */
+        case TOPIC_IS: // topic currently set to
+        this.__buffer[TOPIC_IS] = {
+            '@type': 'update',
+            actor: undefined,
+            target: {
+                '@type': 'room',
+                '@id': 'irc://' + this.server + '/' + channel,
+                displayName: channel
+            },
+            object: {
+                '@type': 'topic',
+                topic: content
+            }
+        };
+        break;
+        case TOPIC_SET_BY: // current topic set by
+        nick = pos3.split('!')[0];
+        this.__buffer[TOPIC_IS].actor = {
+            '@type': 'person',
+            '@id': 'irc://' + nick + '@' + this.server,
+            displayName: nick
+        };
+        this.__buffer[TOPIC_IS].published = msg[0];
+        this.events.emit('stream', this.__buffer[TOPIC_IS]);
+        delete this.__buffer[TOPIC_IS];
+        break;
+
+        /** */
+        default:
+        this.events.emit('unprocessed', string);
+        break;
+    }
+}
+
+module.exports = IrcToActivityStreams;
---
+++ packages/irc2as/test/test-suite.js
@@ -0,0 +1,146 @@
+const fs = require('fs');
+
+if (typeof(define) !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define(['require'], function (require, IRC2AS) {
+    let suites = [];
+    suites.push({
+        name: "irc2astests",
+        desc: "collection of tests for irc2as",
+        abortOnFail: true,
+        setup: function (env, test) {
+            const equal = require('fast-deep-equal');
+            const validStreams = [
+                { 
+                    '@type': 'update',
+                    actor: { 
+                        '@type': 'service',
+                        '@id': 'irc://localhost',
+                        displayName: 'localhost' 
+                    },
+                    object: { 
+                        '@type': 'topic', 
+                        content: [ '-', '-' ] 
+                    }
+                },
+                {
+                    '@type': 'observe',
+                    actor: {
+                        '@type': 'room',
+                        '@id': 'irc://localhost/#kosmos-random',
+                        displayName: '#kosmos-random' 
+                    },
+                    object: {   
+                        '@type': 'attendance',
+                        members: [ 
+                            'hyper_slvrbckt',
+                            'gregkare',
+                            'hal8000',
+                            'botka',
+                            'raucao',
+                            'galfert' 
+                        ]
+                    }
+                },
+                { 
+                    '@type': 'update',
+                    actor: {   
+                        '@type': 'service',
+                        '@id': 'irc://localhost',
+                        displayName: 'localhost' 
+                    },
+                    object: {   
+                        '@type': 'topic',
+                        content: [   
+                            '- on the https://freenode.live website for our call for volunteers and call for',
+                            '- participation. If you are interested in sponsoring next year\'s event, please',
+                            '- send us an e-mail to sponsor@freenode.live',
+                            '-',
+                            '- Thank you for using freenode!',
+                            '-',
+                            '-' 
+                        ] 
+                    }
+                },
+                { '@type': 'update',
+                    actor: 
+                    { '@type': 'person',
+                        '@id': 'irc://donkey2018@localhost',
+                        displayName: 'donkey2018' },
+                    target: 
+                    { '@type': 'person',
+                        '@id': 'irc://slvrbckt@localhost',
+                        displayName: 'slvrbckt' },
+                    object: { '@type': 'address' } 
+                },
+                { '@type': 'update',
+                    actor: 
+                    { '@type': 'person',
+                        '@id': 'irc://slvrbckt@localhost',
+                        displayName: 'slvrbckt' },
+                    target: 
+                    { '@type': 'person',
+                        '@id': 'irc://donkey2018@localhost',
+                        displayName: 'donkey2018' },
+                    object: { '@type': 'address' } 
+                },
+                { '@type': 'leave',
+                    actor: 
+                    { '@type': 'person',
+                        '@id': 'irc://slvrbckt@localhost',
+                        displayName: 'slvrbckt' },
+                    target: 
+                    { '@type': 'room',
+                        '@id': 'irc://localhost/#debian',
+                        displayName: '#debian' },
+                    object: { '@type': 'message', content: 'user has left the channel' } 
+                },
+                {"@type":"leave",actor:{"@type":"person","@id":"irc://jarlaxl_@localhost",displayName:"jarlaxl_"},target:{"@type":"service","@id":"irc://localhost"},object:{"@type":"message",content:"user has quit"}},
+                {"@type":"join",actor:{"@id":"irc://localhost","@type":"service"},object:{"@type":"error",content:"no such channel sdfsdfsdfsdfsdf"},target:{"@id":"irc://sdfsdfsdfsdfsdf@localhost","@type":"person"}},
+                {"@type":"update","actor":{"@type":"person","@id":"irc://lio17@localhost","displayName":"lio17"},"target":{"@type":"room","@id":"irc://localhost/#kosmos-random","displayName":"#kosmos-random"},"object":{"@type":"topic","topic":"testing123"}},
+                {"@type":"update","actor":{"@type":"person","@id":"irc://slvrbckt@localhost","displayName":"slvrbckt"},"object":{"@type":"error","content":"Nickname is already in use."},"target":{"@type":"person","@id":"irc://nkj@localhost","displayName":"nkj"}},
+                {"@type":"send","actor":{"@type":"person","@id":"irc://hyper_slvrbckt@localhost","displayName":"hyper_slvrbckt"},"target":{"displayName":"#kosmos-random"},"object":{"@type":"message","content":"-ssssssss"}},
+                {"@type":"send","actor":{"@type":"room","@id":"irc://localhost/#kosmos-random"},"target":{"@type":"person","@id":"irc://slvrbckt@localhost"},"object":{"@type":"message","content":"You're not a channel operator"}},
+                {"@type":"update","actor":{"@type":"person","@id":"irc://lio17@localhost","displayName":"lio17"},"target":{"@type":"room","@id":"irc://localhost/#kosmos-random","displayName":"#kosmos-random"},"object":{"@type":"topic","topic":"no longer boating in senegal"}},
+                {"@type":"send","actor":{"@type":"person","@id":"irc://raucao@localhost","displayName":"raucao"},"target":{"displayName":"#kosmos-random"},"object":{"@type":"me","content":"is thinking about sending someone to get b33rz"}}
+            ];
+            const IRC2AS = require('./../index');
+            const testData = fs.readFileSync('./test/irc-data.txt', 'utf-8');
+            const irc2as = new IRC2AS({server: 'localhost'});
+
+            irc2as.events.on('stream', (stream) => {
+                delete stream.published;
+                let matched = false;
+                for (let i = 0; i <= validStreams.length; i++) {
+                    matched = equal(stream, validStreams[i]);
+                    if (matched) { 
+                        return; 
+                    }
+                }
+                if (! matched) {
+                    test.fail('failed matching ' + JSON.stringify(stream));
+                }
+            });
+
+            irc2as.events.on('unprocessed', (string) => {
+                console.log('unprocessed> ' + string);
+            });
+
+            testData.split('\n').forEach((line) => {
+                irc2as.input(line);    
+            });
+            test.done();
+        },
+        tests: [
+            {
+                desc: "test", 
+                run: function (env, test) {
+                    test.done();
+                }
+            }
+        ]
+    });
+    return suites;
+});
