90c0d7aeb9ef7631972e95c60febb82bccf2de1e
Nick Jennings <nick@silverbucket.net>
2018-10-11 23:07:45 +0200

    headless browser testing with karma

---
+++ packages/activity-streams.js/karma.config.js
@@ -0,0 +1,22 @@
+module.exports = function (config) {
+  config.set({
+      frameworks: ['mocha', 'chai'],
+      files: [
+          'browser/**/*.js',
+          'test/**/*.js'
+      ],
+      reporters: ['progress'],
+      port: 9876,  // karma web server port
+      colors: true,
+      logLevel: config.LOG_INFO,
+      browsers: ['ChromeHeadless', 'Firefox', 'FirefoxDeveloper', 'FirefoxNightly', 'IE'],
+      autoWatch: false,
+      concurrency: Infinity,
+      customLaunchers: {
+          FirefoxHeadless: {
+              base: 'Firefox',
+              flags: ['-headless'],
+          },
+      },
+  })
+}
