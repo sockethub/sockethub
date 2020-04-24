57334a8dc9c7a556ddc68c26747450bdbd3e04c1
Nick Jennings <nick@silverbucket.net>
2020-04-24 02:15:05 +0200

    aqdd hash function to crypto

--- packages/sockethub/src/crypto.ts
+++ packages/sockethub/src/crypto.ts
@@ -7,15 +7,15 @@ let crypto;
 
 class Crypto {
   constructor() {}
-  encrypt = (json: object, secret: string) => {
+  encrypt(json: object, secret: string) {
     const iv = cryptoLib.randomBytes(IV_LENGTH);
     const cipher = cryptoLib.createCipheriv(ALGORITHM, Buffer.from(secret), iv);
     let encrypted = cipher.update(JSON.stringify(json));
 
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return iv.toString('hex') + ':' + encrypted.toString('hex');
-  };
-  decrypt = (text: string, secret: string) => {
+  }
+  decrypt(text: string, secret: string) {
     let parts = text.split(':');
     const iv = Buffer.from(parts.shift(), 'hex');
     const encryptedText = Buffer.from(parts.join(':'), 'hex');
@@ -24,7 +24,12 @@ class Crypto {
 
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return JSON.parse(decrypted.toString());
-  };
+  }
+  hash(text: string) {
+    const shasum = cryptoLib.createHash('sha1');
+    shasum.update(text);
+    return shasum.digest('hex');
+  }
 }
 
 crypto = crypto ? crypto : new Crypto();
