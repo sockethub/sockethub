1d5d37b5dca2a21daf078071281a49ca4f238e32
Nick Jennings <nick@silverbucket.net>
2020-04-24 02:31:38 +0200

    platform get parentId as another command-line param, and listens on the queue for jobs

--- packages/sockethub/src/platform.ts
+++ packages/sockethub/src/platform.ts
@@ -1,9 +1,15 @@
 import debug from 'debug';
+import services from './services';
+import crypto from "./crypto";
+import SharedResources from "./shared-resources";
+
+const parentId = process.argv[2];
 const platformName = process.argv[3];
-const identifier = process.argv[2];
+const identifier = process.argv[4];
 const logger = debug('sockethub:platform');
 logger(`platform handler initialized for ${platformName} ${identifier}`);
 const PlatformModule = require(`sockethub-platform-${platformName}`);
+const queue = services.startQueue(parentId);
 
 /**
  * Handle any uncaught errors from the platform by alerting the worker and shutting down.
@@ -20,8 +26,10 @@ process.on('uncaughtException', (err) => {
  * method to call, the rest are params.
  */
 process.on('message', (data) => {
+  console.log('incoming message', data);
   let func = data.shift();
   platform[func](...data, (err, obj) => {
+    console.log('sending back job results', err, obj);
     process.send(['callback', err, obj]);
   });
 });
@@ -41,4 +49,23 @@ const platform = new PlatformModule({
   debug: debug(`sockethub:platform:${platformName}:${identifier}`),
   sendToClient: sendFunction('message'),
   updateCredentials: sendFunction('updateCredentials')
+});
+
+queue.process(identifier, (job, done) => {
+  console.log("platform process rceived job ", job.data.msg);
+  // job.data.msg = crypto.decrypt(job.data.msg, parentSecret1 + parentSecret2);
+  // this.log(`got job #${job.id}: ${job.data.msg['@type']}`);
+  //
+  // let identifier = SharedResources.platformMappings.get(job.data.msg.actor['@id']);
+  // const platformInstance = identifier ? SharedResources.platformInstances.get(identifier) :
+  //     this.__getPlatformInstance(job, randToken.generate(16));
+  //
+  // // try to get credentials for this specific secret + socket.id
+  // // (each websocket connection must specify credentials to access initialized platforms)
+  // this.getCredentials(platformInstance, (err, credentials) => {
+  //   if (err) { return done(err); }
+  //   this.jobComplete = done;
+  //   // run corresponding platformInstance method
+  //   platformInstance.process.send([job.data.msg['@type'], job.data.msg, credentials]);
+  // });
 });
