42e18c3acadc51381a8c691ce507d4f52da39468
Nick Jennings <nick@silverbucket.net>
2020-04-26 19:16:42 +0200

    process listens to jobs, and sends results back in the queue

--- packages/sockethub/src/platform.ts
+++ packages/sockethub/src/platform.ts
@@ -2,6 +2,7 @@ import debug from 'debug';
 import services from './services';
 import crypto from "./crypto";
 import SharedResources from "./shared-resources";
+import hash from "object-hash";
 
 const parentId = process.argv[2];
 const platformName = process.argv[3];
@@ -10,6 +11,8 @@ const logger = debug('sockethub:platform');
 logger(`platform handler initialized for ${platformName} ${identifier}`);
 const PlatformModule = require(`sockethub-platform-${platformName}`);
 const queue = services.startQueue(parentId);
+
+let parentSecret1, parentSecret2;
 
 /**
  * Handle any uncaught errors from the platform by alerting the worker and shutting down.
@@ -25,13 +28,13 @@ process.on('uncaughtException', (err) => {
  * Incoming messages from the worker to this platform. Data is an array, the first property is the
  * method to call, the rest are params.
  */
-process.on('message', (data) => {
-  console.log('incoming message', data);
-  let func = data.shift();
-  platform[func](...data, (err, obj) => {
-    console.log('sending back job results', err, obj);
-    process.send(['callback', err, obj]);
-  });
+process.on('message', (msg) => {
+  console.log('incoming IPC message: ' + msg.type, msg.data);
+  if (msg.type === 'secrets') {
+    parentSecret1 = msg.data.parentSecret1;
+    parentSecret2 = msg.data.parentSecret2;
+    startQueueListener()
+  }
 });
 
 /**
@@ -45,27 +48,57 @@ function sendFunction(command) {
   };
 }
 
+// TODO handle credential fetching
+// function getCredentials(id, cb) {
+//   this.store.get(platformInstance.actor['@id'], (err, credentials) => {
+//     if (platformInstance.config.persist) {
+//       // don't continue if we don't get credentials
+//       if (err) { return cb(err); }
+//       this.persistPlatformInstance(platformInstance);
+//     }
+//
+//     if (platformInstance.credentialsHash) {
+//       if (platformInstance.credentialsHash !== hash(credentials.object)) {
+//         return cb('provided credentials do not match existing platform instance for actor '
+//             + platformInstance.actor['@id']);
+//       }
+//     } else {
+//       platformInstance.credentialsHash = hash(credentials.object);
+//     }
+//     cb(undefined, credentials);
+//   });
+// }
+
 const platform = new PlatformModule({
   debug: debug(`sockethub:platform:${platformName}:${identifier}`),
   sendToClient: sendFunction('message'),
   updateCredentials: sendFunction('updateCredentials')
 });
 
-queue.process(identifier, (job, done) => {
-  console.log("platform process rceived job ", job.data.msg);
-  // job.data.msg = crypto.decrypt(job.data.msg, parentSecret1 + parentSecret2);
-  // this.log(`got job #${job.id}: ${job.data.msg['@type']}`);
-  //
-  // let identifier = SharedResources.platformMappings.get(job.data.msg.actor['@id']);
-  // const platformInstance = identifier ? SharedResources.platformInstances.get(identifier) :
-  //     this.__getPlatformInstance(job, randToken.generate(16));
-  //
-  // // try to get credentials for this specific secret + socket.id
-  // // (each websocket connection must specify credentials to access initialized platforms)
-  // this.getCredentials(platformInstance, (err, credentials) => {
-  //   if (err) { return done(err); }
-  //   this.jobComplete = done;
-  //   // run corresponding platformInstance method
-  //   platformInstance.process.send([job.data.msg['@type'], job.data.msg, credentials]);
-  // });
-});
+function startQueueListener() {
+  console.log('starting queue listener');
+  queue.process(identifier, (job, done) => {
+    console.log("platform process received job ", job.data.msg);
+    job.data.msg = crypto.decrypt(job.data.msg, parentSecret1 + parentSecret2);
+    console.log("platform process decreypted job ", job.data.msg);
+    platform[job.data.msg['@type']](job.data.msg, credentials, done);
+  });
+}
+
+// job.data.msg = crypto.decrypt(job.data.msg, parentSecret1 + parentSecret2);
+// this.log(`got job #${job.id}: ${job.data.msg['@type']}`);
+//
+// let identifier = SharedResources.platformMappings.get(job.data.msg.actor['@id']);
+// const platformInstance = identifier ? SharedResources.platformInstances.get(identifier) :
+//     this.__getPlatformInstance(job, randToken.generate(16));
+//
+// // try to get credentials for this specific secret + socket.id
+// // (each websocket connection must specify credentials to access initialized platforms)
+// this.getCredentials(platformInstance, (err, credentials) => {
+//   if (err) { return done(err); }
+//   this.jobComplete = done;
+//   // run corresponding platformInstance method
+//   platformInstance.process.send([job.data.msg['@type'], job.data.msg, credentials]);
+// });
+
+console.log('process started')
