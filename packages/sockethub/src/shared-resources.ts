84fa1c808d6fe5dabf87331b7d5276e3c040cb75
Nick Jennings <nick@silverbucket.net>
2020-06-14 21:55:20 +0200

    no more need for platformMappings, removePlatform improvements

--- packages/sockethub/src/shared-resources.ts
+++ packages/sockethub/src/shared-resources.ts
@@ -1,14 +1,17 @@
 const SharedResources = {
-  // collection of platform instances, stored by `uid`
+  // collection of platform instances, stored by `id`
   platformInstances: (() => new Map())(),
-  // collection of socket instances, stored by `socket.id`
-  socketConnections: (() => new Map())(),
-  // mappings between actor `uri` and platform instance `uid`
-  platformMappings: (() => new Map())(),
+  // collection of socket instances (referred to internally as session),
+  // stored as `socket.id` from socket.io
+  sessionConnections: (() => new Map())(),
   helpers: {
     removePlatform: function (platformInstance) {
-      SharedResources.platformMappings.delete(platformInstance.actor['@id']);
       SharedResources.platformInstances.delete(platformInstance.id);
+      platformInstance.process.removeAllListeners('close');
+      try {
+        platformInstance.process.unref();
+        platformInstance.process.kill();
+      } catch (e) { console.log(e); }
     }
   }
 };
