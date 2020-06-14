0a112520c614cec77080c2f72181e738a9d14ca5
Nick Jennings <nick@silverbucket.net>
2020-06-14 21:51:39 +0200

    refactor to use process instance

--- packages/sockethub/src/process-manager.ts
+++ packages/sockethub/src/process-manager.ts
@@ -1,7 +1,7 @@
 import init from './bootstrap/init';
 import SharedResources from "./shared-resources";
-import crypto from "./crypto";
-import { fork } from "child_process";
+import PlatformInstance from "./platform-instance";
+import { getPlatformId } from "./common";
 
 class ProcessManager {
   private readonly parentId: string;
@@ -14,63 +14,30 @@ class ProcessManager {
     this.parentSecret2 = parentSecret2;
   }
 
-  register(msg: any) {
+  register(msg: any, sessionId: string) {
     const platformDetails = init.platforms.get(msg.context);
 
-    if (! platformDetails.config.persist) {
+    if (platformDetails.config.persist) {
+      // ensure process is started - one for each actor
+      return this.ensureProcess(msg.context, msg.actor['@id'], sessionId);
+    } else {
       // ensure process is started - one for all jobs
       return this.ensureProcess(msg.context);
-    } else {
-      // ensure process is started - one for each actor
-      return this.ensureProcess(msg.context, msg.actor['@id']);
     }
   }
   
-  private ensureProcess(platform: string, actor?: string, credentials?: object) {
-    const identifier = actor ? crypto.hash(platform + actor) : crypto.hash(platform);
+  private ensureProcess(platform: string, actor?: string, sessionId?: string) {
+    const identifier = getPlatformId(platform, actor);
+    const platformInstance = SharedResources.platformInstances.get(identifier) ||
+        new PlatformInstance(identifier, platform, this.parentId);
 
-    if (SharedResources.platformInstances.has(identifier)) {
-      return identifier;
-    }
+    platformInstance.registerSession(sessionId);
 
-    // spin off a process
-    const childProcess = fork('dist/platform.js', [this.parentId, platform, identifier]);
-
-    const platformInstance = {
-      id: identifier,
-      name: platform,
-      actor: actor,
-      config: { persist: true },
-      sendToClient: (e) => {
-        console.log('sendToClient called ', e);
-      },
-      process: childProcess,
-      credentialsHash: undefined,
-      flaggedForTermination: false,
-      sockets: new Set()
-    };
-
-    childProcess.on('close', (e) => {
-      console.log('close even triggered');
-      platformInstance.sendToClient({
-        context: platformInstance.name,
-        '@type': 'error',
-        target: platformInstance.actor,
-        object: {
-          '@type': 'error',
-          content: e
-        }
-      });
-      console.log('remove platform instance ' + platformInstance.id);
-      SharedResources.helpers.removePlatform(platformInstance);
-    });
-
-    childProcess.send({
+    platformInstance.process.send({
       type: 'secrets',
       data: {
         parentSecret1: this.parentSecret1,
-        parentSecret2: this.parentSecret2,
-        credentials: credentials
+        parentSecret2: this.parentSecret2
       }
     });
     
