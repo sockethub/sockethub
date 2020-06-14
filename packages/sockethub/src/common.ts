b79d6c214906cdb478720d413e915261e8fd5b32
Nick Jennings <nick@silverbucket.net>
2020-06-14 21:39:36 +0200

    common functions

---
+++ packages/sockethub/src/common.ts
@@ -0,0 +1,18 @@
+import Store from 'secure-store-redis';
+
+import config from "./config";
+import crypto from "./crypto";
+
+export function getSessionStore(parentId: string, parentSecret: string,
+                                sessionId: string, sessionSecret: string) {
+  return new Store({
+    namespace: 'sockethub:' + parentId + ':session:' + sessionId + ':store',
+    secret: parentSecret + sessionSecret,
+    redis: config.get('redis')
+  });
+}
+
+export function getPlatformId(platform: string, actor?: string) {
+  return actor ? crypto.hash(platform + actor) : crypto.hash(platform);
+}
+
