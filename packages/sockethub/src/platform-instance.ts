b2cfbded1aef5def842ac438435d296c29843f53
Nick Jennings <nick@silverbucket.net>
2020-06-14 21:42:56 +0200

    new platform instance class which handles the registering and deregistering of listeners and references the child process

---
+++ packages/sockethub/src/platform-instance.ts
@@ -0,0 +1,83 @@
+import { ChildProcess, fork } from 'child_process';
+
+import SharedResources from "./shared-resources";
+
+class PlatformInstance {
+  flaggedForTermination: boolean = false;
+  readonly id: string;
+  readonly name: string;
+  readonly process: ChildProcess;
+  readonly parentId: string;
+  readonly sessions: Set<string> = new Set();
+  private readonly actor?: string;
+  private readonly listeners: object = {
+    'close': (() => new Map())(),
+    'message': (() => new Map())(),
+  };
+
+  constructor(id: string, name: string, parentId: string, actor?: string) {
+    this.id = id;
+    this.name = name;
+    this.parentId = parentId;
+    this.actor = actor;
+    // spin off a process
+    this.process = fork('dist/platform.js', [parentId, name, id]);
+  }
+
+  public registerSession(sessionId: string) {
+    if (! this.sessions.has(sessionId)) {
+      this.sessions.add(sessionId);
+      this.registerListeners(sessionId);
+    }
+  }
+  
+  public deregisterSession(sessionId: string) {
+    SharedResources.helpers.removePlatform(this);
+    this.sessions.delete(sessionId);
+    this.deregisterListeners(sessionId);
+  }
+
+  public sendToClient(sessionId: string, msg: any) {
+    // XXX TODO
+    console.log('sendToClient called ', msg);
+  }
+
+  private deregisterListeners(sessionId: string) {
+    for (let key of Object.keys(this.listeners)) {
+      this.process.removeListener(key, this.listeners[key].get(sessionId));
+      this.listeners[key].delete(sessionId);
+    }
+  }
+
+  private registerListeners(sessionId: string) {
+    for (let key of Object.keys(this.listeners)) {
+      const listenerFunc = this.listenerFunction(key, sessionId);
+      this.process.on(key, listenerFunc);
+      this.listeners[key].set(sessionId, listenerFunc);
+    }
+  }
+
+  private listenerFunction(key, sessionId) {
+    const funcs = {
+      'close': (e) => {
+        console.log('close even triggered ' + this.id);
+        this.sendToClient(sessionId,{
+          context: this.name,
+          '@type': 'error',
+          target: this.actor || {},
+          object: {
+            '@type': 'error',
+            content: e
+          }
+        });
+        this.deregisterSession(sessionId);
+      },
+      'message': (msg) => {
+        this.sendToClient(sessionId, msg);
+      }
+    };
+    return funcs[key];
+  }
+}
+
+export default PlatformInstance;
