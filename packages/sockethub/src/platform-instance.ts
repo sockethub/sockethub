3f2ba07d116441a81aa507ba94c04488a99a4f06
Garret Alfert <alfert@wevelop.de>
2020-07-30 14:10:36 +0200

    Use relative path for process forking
    Fixes #255
    
    This fixes an error (`MODULE_NOT_FOUND`) when running Sockethub as a dependency of another project.

--- packages/sockethub/src/platform-instance.ts
+++ packages/sockethub/src/platform-instance.ts
@@ -1,4 +1,5 @@
 import { ChildProcess, fork } from 'child_process';
+import { join } from 'path';
 
 import SharedResources from "./shared-resources";
 
@@ -38,7 +39,7 @@ class PlatformInstance {
       this.actor = actor;
     }
     // spin off a process
-    this.process = fork('dist/platform.js', [parentId, name, id]);
+    this.process = fork(join(__dirname, 'platform.js'), [parentId, name, id]);
   }
 
   public registerSession(sessionId: string) {
