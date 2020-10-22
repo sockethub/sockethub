933a95a925596e81f2f470c6186aca1883914bdb
Nick Jennings <nick@silverbucket.net>
2020-10-22 17:41:56 +0200

    build file

--- packages/sockethub/dist/resource-manager.js
+++ packages/sockethub/dist/resource-manager.js
@@ -35,6 +35,7 @@ function resourceManagerCycle() {
             }
             if (platformInstance.global) {
                 // static platform for global use, don't do resource management
+                continue;
             }
             else if (platformInstance.sessions.size <= 0) {
                 if (platformInstance.flaggedForTermination) {
