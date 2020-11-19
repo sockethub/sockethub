26edb29c9b64ca6ee60bc1bf53498162eab1f264
Nick Jennings <nick@silverbucket.net>
2020-11-19 16:19:46 +0100

    Fix multiple listeners on the job queue (#290)
    * decouple init process from config loading
    
    * ensureProcess should only send session secrets once upon creation and not during every check to prevent multiple queue listeners being initialized in the platform.ts (which also now double checks for whether the queue has already been started

--- packages/sockethub/dist/common.js
+++ packages/sockethub/dist/common.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getPlatformId = exports.getSessionStore = void 0;
 const secure_store_redis_1 = __importDefault(require("secure-store-redis"));
 const config_1 = __importDefault(require("./config"));
 const crypto_1 = __importDefault(require("./crypto"));
