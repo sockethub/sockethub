1b41b37e7bafaca99c6267c03b8e82d4e06806a6
Nick Jennings <nick@silverbucket.net>
2021-03-14 00:13:45 +0100

    update dummy platform to be more useful for debugging

--- packages/sockethub-platform-dummy/index.js
+++ packages/sockethub-platform-dummy/index.js
@@ -1,51 +1,47 @@
-const packageJSON = require('./package.json'),
-      request = require('request');
+const packageJSON = require('./package.json');
 
-function Dummy(cfg) {
-  cfg = (typeof cfg === 'object') ? cfg : {};
-  this.id = cfg.id;
-  this.debug = cfg.debug;
-  this.sendToClient = cfg.sendToClient;
+class Dummy {
+  constructor(cfg) {
+    cfg = (typeof cfg === 'object') ? cfg : {};
+    this.id = cfg.id;
+    this.debug = cfg.debug;
+    this.sendToClient = cfg.sendToClient;
+  }
+
+  get schema() {
+    return {
+      version: packageJSON.version,
+      messages: {
+        "required": ["@type"],
+        "properties": {
+          "@type": {
+            "enum": ["echo", "fail"]
+          }
+        }
+      },
+      credentials: {}
+    };
+  }
+
+  get config() {
+    return {
+      persist: false,
+      requireCredentials: []
+    };
+  };
+
+  echo(job, cb) {
+    this.sendToClient(job.object.content);
+    cb();
+  };
+
+  fail(job, cb) {
+    cb(new Error(job.object.content));
+  };
+
+  cleanup(cb) {
+    cb();
+  };
 }
-
-Dummy.prototype.schema = {
-  version: packageJSON.version,
-  messages: {},
-  credentials: {}
-};
-
-Dummy.prototype.config = {};
-
-Dummy.prototype.fetch = function (job, credentials, cb) {
-  this.debug('using request on ' + job.target.id);
-  request(job.target.id)
-    .on('error', function (err) {
-      this.debug('error received: ', err);
-      cb(err);
-    })
-
-    .on('response', function (resp) {
-      this.debug('response received');
-      //this.sendToClient(resp);
-      //cb(null);
-    })
-
-    .on('end', function () {
-      this.debug('end event');
-      cb(null);
-    });
-};
-
-Dummy.prototype.send = function (job, credentials, cb) {
-  cb();
-};
-
-Dummy.prototype.error = function (job, credentials, cb) {
-  cb();
-};
-
-Dummy.prototype.cleanup = function (cb) {
-  cb();
-};
 
 module.exports = Dummy;
