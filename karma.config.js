d03e300f5458f25f308aa1113247bbf5d2c2da32
Nick Jennings <nick@silverbucket.net>
2021-05-24 18:29:05 +0200

    remove old cruft

--- karma.config.js
+++ karma.config.js
@@ -2,20 +2,13 @@ module.exports = function (config) {
   config.set({
     frameworks: ['mocha', 'chai'],
     files: [
-      // 'packages/sockethub/node_modules/socket.io/client-dist/socket.io.min.js',
-      // 'packages/activity-streams.js/browser/activity-streams.min.js',
-      // 'packages/sockethub/dist/js/client.js',
-      // 'http://localhost:10550/sockethub/socket.io.js',
-      // 'http://localhost:10550/activity-streams.min.js',
-      // 'http://localhost:10550/sockethub-client.js',
       'test/**/*.test.js',
     ],
     reporters: [ 'progress' ],
     port: 9876,  // karma web server port
     colors: true,
     logLevel: config.LOG_INFO,
-    // browsers: ['ChromeHeadless', 'Firefox', 'FirefoxDeveloper', 'FirefoxNightly', 'IE'],
-    browsers: ['Firefox'],
+    browsers: ['ChromeHeadless', 'Firefox', 'FirefoxDeveloper', 'FirefoxNightly', 'IE'],
     autoWatch: false,
     concurrency: Infinity,
     customLaunchers: {
@@ -23,14 +16,6 @@ module.exports = function (config) {
         base: 'Firefox',
         flags: ['-headless'],
       },
-    // },
-    // client: {
-    //   karmaHTML: {
-    //     auto: true,
-    //     source: [
-    //       { src: 'test/index.html', tag: 'index' }
-    //     ]
-    //   }
     }
   });
 }
