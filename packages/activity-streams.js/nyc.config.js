c3781fbd92f9986852e1cf64f897bd87fb7cf80d
Nick Jennings <nick@silverbucket.net>
2021-08-04 00:07:03 +0200

    switch to nyc from c8 and add coverage thresholes

---
+++ packages/activity-streams.js/nyc.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "all": true,
+  "include": [
+    "src/**/*.js"
+  ],
+  "exclude": [
+    "src/**/*.test.js"
+  ],
+  "statements": 95,
+  "branches": 85,
+  "functions": 85,
+  "lines": 95
+};
