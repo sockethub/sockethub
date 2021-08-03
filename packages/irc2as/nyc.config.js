7aa503834c0de985ad2d5a5be2a65d9579a4f3af
Nick Jennings <nick@silverbucket.net>
2021-08-04 00:12:25 +0200

    switch to nyc from c8 and set coverage thresholds

---
+++ packages/irc2as/nyc.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "all": true,
+  "include": [
+    "**/*.js"
+  ],
+  "exclude": [
+    "**/*-suite.js",
+    "nyc.config.js"
+  ],
+  "statements": 90,
+  "branches": 90,
+  "functions": 90,
+  "lines": 90
+};
