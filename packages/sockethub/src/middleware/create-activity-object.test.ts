9b236b40564b1d9c885698bcee69a94523142484
Nick Jennings <nick@silverbucket.net>
2021-08-27 13:43:58 +0200

    createActivityObject tests

---
+++ packages/sockethub/src/middleware/create-activity-object.test.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+import proxyquire from 'proxyquire';
+import * as sinon from 'sinon';
+
+proxyquire.noPreserveCache();
+proxyquire.noCallThru();
+
+describe('Middleware: createActivityObject', () => {
+  let sandbox, createStub, createActivityObject;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    createStub = sinon.stub();
+    const createActivityObjectMod = proxyquire('./create-activity-object', {
+      'activity-streams': sinon.stub().returns({
+        Object: {
+          create: createStub
+        }
+      }),
+      '../config': {
+        get: sinon.stub()
+      }
+    });
+    createActivityObject = createActivityObjectMod.default;
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+  it('Calls activity.Object.create with incoming data', () => {
+    createActivityObject({foo: 'bar'}, () => {
+      sandbox.assert.calledWith(createStub, {foo: 'bar'});
+    })
+  });
+});
