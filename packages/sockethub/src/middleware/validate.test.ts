06903b752dafa007c0bf984c50209edaeec12532
Nick Jennings <nick@silverbucket.net>
2021-08-27 22:45:41 +0200

    linting

--- packages/sockethub/src/middleware/validate.test.ts
+++ packages/sockethub/src/middleware/validate.test.ts
@@ -7,13 +7,13 @@ proxyquire.noCallThru();
 // @ts-ignore
 import platformLoad from './../bootstrap/platforms';
 const packageJSON = require('./../../package.json');
-const platforms = platformLoad(Object.keys(packageJSON.dependencies))
+const platforms = platformLoad(Object.keys(packageJSON.dependencies));
 
 const validateMod = proxyquire('./validate', {
   '../bootstrap/init': {
     platforms: platforms
   }
-})
+});
 const validate = validateMod.default;
 
 import asObjects from "./validate.test.data";
