ca42964b96d3e429469b551ba3238824eac817e0
Nick Jennings <nick@silverbucket.net>
2022-01-16 15:36:39 -0600

    cleanup middleware

--- packages/sockethub/src/middleware.ts
+++ packages/sockethub/src/middleware.ts
@@ -1,12 +1,5 @@
 import { debug } from 'debug';
 
-/**
- * Returns a function which accepts a list of middleware functions to call, in order.
- * The errorHandler will be called whenever an error occurs.
- * @return {function} chains a series of functions to be called in sequence
- */
-
-
 export default function middleware(name: string): MiddlewareChain {
   return new MiddlewareChain(name);
 }
@@ -57,44 +50,3 @@ export class MiddlewareChain {
     };
   }
 }
-
-
-/**
- * When calling the middleware instance function, pass in a list of functions to call in order.
- * Each function can expect any number of params, but the final param should expect a callback.
- *
- * After all functions are completed, it will call the callback function. If any middleware calls
- * the callback with the first parameter an instance of Error, then the call stack will be aborted,
- * and the final callback (callback of first input) will be called with the Error as the first i
- * parameter.
- * Additionally, if provided during the `createMiddleware` call, the errorHandler will be called,
- * with the error object as the first parameter along with the remaining parameters.
- *
- * @param {function} middleware Any number of functions, each expecting a callback as the final
- * parameter.
- * @return {function} entry The entry function, which will start calling functions defined in the
- * chain.
- *
- * @example
- *
- *  const entry = createMiddleware(errorHandler)(
- *    (data, cb) => {
- *      //... do something with data
- *      cb(data);
- *    },
- *    (data, cb) => {
- *      //... do more with data
- *      cb(data);
- *    },
- *    (data, cb) => {
- *      //... do last stuff with data
- *      cb();
- *    }
- *  );
- *
- *  entry(initialData, (err, data) => {
- *    // this function is called when complete or an error occurs anywhere on the chain
- *    // in which case execution is aborted.
- *  });
- *
- */
