11c020a432cc41471fa6536de15ecc00707cc7a1
Nick Jennings <nick@silverbucket.net>
2022-03-05 18:52:11 +0100

    delete old dirs

--- packages/sockethub-schemas/index.js
+++
@@ -1,9 +0,0 @@
-const debug = require('debug');
-const log = debug('sockethub:schemas');
-log('loading sockethub activity stream schemas');
-module.exports = {
-  ActivityStream: require('./schemas/activity-stream.js'),
-  ActivityObject: require('./schemas/activity-object.js'),
-  platform: require('./src/platform.js'),
-  validator: require('./src/validator.js')
-};
