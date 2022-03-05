078219732601fcaf4d90fcbe4e172437c4c378da
Nick Jennings <nick@silverbucket.net>
2022-03-05 18:51:43 +0100

    @sockethub/server

--- packages/sockethub/src/bootstrap/init.d.ts
+++
@@ -1,8 +0,0 @@
-declare const init: {
-    version: any;
-    platforms: Map<any, any>;
-    host: any;
-    port: any;
-    path: any;
-};
-export default init;
--- packages/sockethub/src/janitor.d.ts
+++
@@ -1,8 +0,0 @@
-declare function janitorCycle(): void;
-declare const janitor: {
-    start: typeof janitorCycle;
-    alreadyCalled: boolean;
-    cycleCount: number;
-    reportCount: number;
-};
-export default janitor;
