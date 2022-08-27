e52879ea8ef3a3744832ef3592b47f4985c1ced9
Nick Jennings <nick@silverbucket.net>
2022-08-27 18:53:50 +0200

    cleanup

--- packages/crypto/src/index.test.ts
+++ packages/crypto/src/index.test.ts
@@ -16,29 +16,35 @@ describe('crypto', () => {
         randomBytes: () => Buffer.alloc(16)
       }
     }).default;
+  });
 
-  })
   it('encrypts', () => {
     expect(crypto.encrypt(data, secret)).to.be.equal(encryptedData);
   });
+
   it('decrypts', () => {
     expect(crypto.decrypt(encryptedData, secret)).to.eql(data);
   });
+
   it('hashes', () => {
     expect(crypto.hash('foobar')).to.be.equal('8843d7f');
   });
+
   it('randTokens 8', () => {
     const token = crypto.randToken(8);
     expect(token.length).to.be.equal(8);
   });
+
   it('randTokens 16', () => {
     const token = crypto.randToken(16);
     expect(token.length).to.be.equal(16);
   });
+
   it('randTokens 32', () => {
     const token = crypto.randToken(32);
     expect(token.length).to.be.equal(32);
   });
+
   it('randTokens 33+ will fail', () => {
     expect(() => {
       crypto.randToken(33);
