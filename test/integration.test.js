a9afd781ad71f4751830705d64e06cdd34f18dad
Nick Jennings <nick@silverbucket.net>
2022-08-29 13:40:36 +0200

    improve feeds tests, fix brief vs normal texts

--- test/integration.test.js
+++ test/integration.test.js
@@ -75,69 +75,80 @@ describe('Page', () => {
     });
 
     describe('Feeds', () => {
-      it(`fetch an XML feed`, (done) => {
-        sc.socket.emit('message', {
-          context: "feeds",
-          type: "fetch",
-          actor: {
-            type: "website",
-            id: "https://sockethub.org/examples/feeds"
-          },
-          target: {
-            type: 'feed',
-            id: 'http://localhost:10550/examples/feed.xml'
-          }
-        }, (msg) => {
-          console.log(`Feed fetch callback! `, msg);
-          expect(msg.length).to.eql(20);
+      describe('fetches a feed', () => {
+        it(`gets expected results`, (done) => {
+          sc.socket.emit('message', {
+            context: "feeds",
+            type: "fetch",
+            actor: {
+              type: "website",
+              id: "https://sockethub.org/examples/feeds"
+            },
+            target: {
+              type: 'feed',
+              id: 'http://localhost:10550/examples/feed.xml'
+            }
+          }, (msg) => {
+            console.log(`Feed fetch callback!`);
+            expect(msg.length).to.eql(20);
+            msg.forEach((m) => {
+              expect(m.object.text.length >= m.object.brief_text.length).to.be.true;
+            }).finally(done)
+          });
+        });
+      });
+    });
+
+    describe('XMPP', () => {
+      describe('emit on credentials channel', () => {
+        it('successfully fires callback', (done) => {
+          sc.socket.emit('credentials', {
+            actor: {
+              id: "jimmy@prosody/SockethubExample",
+              type: 'person'
+            },
+            context: "xmpp",
+            type: "credentials",
+            object: {
+              type: "credentials",
+              password: "passw0rd",
+              resource: "SockethubExample",
+              userAddress: "jimmy@prosody"
+            }
+          }, (msg) => {
+            console.log('XMPP credentials callback! ', msg);
+            if (msg?.error) { done(msg.error); }
+            else { done(); }
+          });
+        });
+      });
+
+      describe('ActivityStreams.create', () => {
+        it('successfully creates and stores an activity-object', (done) => {
+          const actor = {
+            id: "jimmy@prosody/SockethubExample",
+            type: "person",
+            name: "Jimmy Userson"
+          };
+          const obj = sc.ActivityStreams.Object.create(actor);
+          expect(sc.ActivityStreams.Object.get(actor.id)).to.eql(actor);
+          console.log('activity-object create result: ', obj);
           done();
         });
       });
-    })
+      
 
-    describe('XMPP', () => {
-      it('sends credentials', (done) => {
-        sc.socket.emit('credentials', {
-          actor: {
-            id: "jimmy@prosody/SockethubExample",
-            type: 'person'
-          },
-          context: "xmpp",
-          type: "credentials",
-          object: {
-            type: "credentials",
-            password: "passw0rd",
-            resource: "SockethubExample",
-            userAddress: "jimmy@prosody"
-          }
-        }, (msg) => {
-          console.log('XMPP credentials callback! ', msg);
-          if (msg?.error) { done(msg.error); }
-          else { done(); }
-        });
-      });
-
-      it('creates activity-object', (done) => {
-        const actor = {
-          id: "jimmy@prosody/SockethubExample",
-          type: "person",
-          name: "Jimmy Userson"
-        };
-        const obj = sc.ActivityStreams.Object.create(actor);
-        expect(sc.ActivityStreams.Object.get(actor.id)).to.eql(actor);
-        console.log('activity-object create result: ', obj);
-        done();
-      });
-
-      it('sends connect', (done) => {
-        sc.socket.emit('message', {
-          type: "connect",
-          actor: "jimmy@prosody/SockethubExample",
-          context: "xmpp"
-        }, (msg) => {
-          console.log('XMPP message callback! ', msg);
-          if (msg?.error) { done(msg.error); }
-          else { done(); }
+      describe('connect', () => {
+        it('sends successfully with callback fired', (done) => {
+          sc.socket.emit('message', {
+            type: "connect",
+            actor: "jimmy@prosody/SockethubExample",
+            context: "xmpp"
+          }, (msg) => {
+            console.log('XMPP message callback! ', msg);
+            if (msg?.error) { done(msg.error); }
+            else { done(); }
+          });
         });
       });
     });
