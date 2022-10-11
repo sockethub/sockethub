776959e0b14fa8fc07a77ba37f283911165ece38
Nick Jennings <nick@silverbucket.net>
2022-10-11 20:32:38 +0200

    layout work and styling

--- packages/examples/src/routes/dummy/+page.svelte
+++ packages/examples/src/routes/dummy/+page.svelte
@@ -2,36 +2,63 @@
 	{@html edgeLight}
 </svelte:head>
 
-<h1>Dummy Example</h1>
-<div class="px-4 py-2">
-<p>The dummy platform is the most basic test to communicate via Sockethub to a platform, and receive a response back.</p>
-<p>You can use either the echo or fail types on your Activity Stream object.</p>
+<div class="grid grid-cols-8 gap-4">
+<div class="col-start-3 col-end-7">
+		<h1>Dummy Example</h1>
+		<div class="px-4 py-2">
+			<p>The dummy platform is the most basic test to communicate via Sockethub to a platform, and receive a response back.</p>
+			<p>You can use either the echo or fail types on your Activity Stream object.</p>
+		</div>
 </div>
-<div class="py-4">
-	<h2 class="py-2">Message Content</h2>
-	<input bind:value={content} class="border-4" placeholder="Text to send as content">
-	<h2 class="py-2">Send Object Type</h2>
-	<button on:click={sendEcho} class="hover:bg-blue-700 bg-blue-500 text-white font-bold py-2 px-4 rounded" disabled="{!online}">Echo</button>
-	<button on:click={sendFail} class="hover:bg-blue-700 bg-blue-500 text-white font-bold py-2 px-4 rounded" disabled="{!online}">Fail</button>
-</div>
-<div class="py-4">
-	<h2 class="py-2">Response from Sockethub</h2>
-	<div id="messages">
-		<ul>
-			{#each messages as msg}
-				<li>
-					<button on:click="{showLog(msg.id)}" data-modal-toggle="defaultModal" class="hover:bg-blue-400 bg-blue-300 text-black py-0 px-2 rounded mr-3 mb-1">log</button>
-					<span>#{msg.id} {msg.actor.id}</span> [<span>{msg.type}</span>]: <span>{msg.object.content}</span>
-					{#if msg.error}
-						<span class="ml-5 text-red-500">{msg.error}</span>
-					{/if}
-				</li>
-			{/each}
-		</ul>
+<div class="col-start-3 col-end-7 grow">
+	<div class="flex gap-4 grow">
+		<div class="w-1/4 ml-20">
+			<h2 class="py-2">Message Content</h2>
+			<input bind:value={content} class="border-4" placeholder="Text to send as content">
+		</div>
+		<div class="w-1/4">
+			<h2 class="py-2">Send Object Type</h2>
+			<div class="flex gap-4">
+				<button on:click={sendEcho}
+								class="hover:bg-blue-700 bg-blue-500 text-white font-bold py-2 px-4 rounded"
+								disabled="{!online}">Echo</button>
+				<button on:click={sendFail}
+								class="hover:bg-blue-700 bg-blue-500 text-white font-bold py-2 px-4 rounded"
+								disabled="{!online}">Fail</button>
+			</div>
+		</div>
+		<div>
+			<h2 class="py-2">Status</h2>
+			<div class="pt-1 pl-2 pr-2 pb-1.5 rounded-xl text-white font-bold {online ? 'bg-green-300' : 'bg-gray-300 '}">{online ? 'connected' : 'disconnected'}</div>
+		</div>
 	</div>
 </div>
+<div class="col-start-3 col-end-7 grow">
+	<div class="py-4 grow">
+		<div class=" ml-20">
+			<h2 class="py-2">Response from Sockethub</h2>
+			<div id="messages">
+				<ul>
+					{#each messages as msg}
+						<li>
+							<button on:click="{showLog(msg.id)}"
+											data-modal-toggle="defaultModal"
+											class="hover:bg-blue-400 bg-blue-300 text-black py-0 px-2 rounded mr-3 mb-1">view log</button>
+							<span>#{msg.id} {msg.actor.id}</span> [<span>{msg.type}</span>]: <span>{msg.object.content}</span>
+							{#if msg.error}
+								<span class="ml-5 text-red-500">{msg.error}</span>
+							{/if}
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
 <div class="{logModalState ? '' : 'hidden'} bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-	<div style="background-color: #fafafa;" class="px-2 py-2 rounded-md text-left">
+	<div style="" class="bg-offwhite px-2 py-2 rounded-md text-left">
 		<div class="flex flex-row">
 			<div class="text-xs mb-4 text-slate-500 font-mono py-1 basis-1/2">
 				<h2>Sent</h2>
--- packages/examples/tailwind.config.cjs
+++ packages/examples/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "offwhite": "#fafafa"
+      }
+    },
   },
   plugins: [],
 }
