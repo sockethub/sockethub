4c72534cff859ded5b39f579865cd89d44197130
Nick Jennings <nick@silverbucket.net>
2022-12-07 13:18:48 +0100

    fix up feeds

--- packages/examples/src/routes/feeds/+page.svelte
+++ packages/examples/src/routes/feeds/+page.svelte
@@ -3,42 +3,35 @@
   <p>The feeds platform fetches takes an RSS/ATOM feed URL and parses it, returning an array of Activity Objects for each entry.</p>
 </Intro>
 
-<Module>
-  <ActivityActor actor={actor} />
-</Module>
+<ActivityActor actor={actor} />
 
-<Module>
-  <div class="w-16 md:w-32 lg:w-48 grow w-full">
-    <label for="URL" class="form-label inline-block text-gray-900 font-bold mb-2">Feed URL</label>
-    <input id="URL" bind:value={url} class="border-4 grow w-full">
+<div>
+  <div class="w-full p-2">
+    <label for="URL" class="inline-block text-gray-900 font-bold w-32">Feed URL</label>
+    <input id="URL" bind:value={url} class="border-4">
   </div>
-  <div class="w-16 md:w-32 lg:w-48 w-full">
-    <div class="flex gap-4 mt-6">
-      <div>
-        <SockethubButton disabled={!$actor.isSet} buttonAction={sendFetch}>Fetch</SockethubButton>
-      </div>
-    </div>
+  <div class="w-full text-right">
+    <SockethubButton disabled={!$actor.state.actorSet} buttonAction={sendFetch}>Fetch</SockethubButton>
   </div>
-</Module>
+</div>
 
-<Module>
-  <Logger />
-</Module>
+<Logger />
 
 <script lang="ts">
   import Intro from "$components/Intro.svelte";
-  import Module from "$components/Module.svelte";
   import ActivityActor from "$components/ActivityActor.svelte";
   import SockethubButton from "$components/SockethubButton.svelte";
   import Logger, { addObject, ObjectType } from "$components/logs/Logger.svelte";
   import { sc } from "$lib/sockethub";
   import { getActorStore } from "$stores/ActorStore";
 
-  const defaultActorId = 'https://sockethub.org/examples/feedsUser';
+  const actorId = 'https://sockethub.org/examples/feedsUser';
   const actor = getActorStore({
-    isSet: false,
+    state: {
+      actorSet: false
+    },
     object: {
-      id: defaultActorId,
+      id: actorId,
       type: "person",
       name: "Sockethub Examples Feeds"
     }
@@ -64,7 +57,7 @@
     return {
       context: "feeds",
       type: type,
-      actor: defaultActorId,
+      actor: actorId,
       target: {
         type: "feed",
         id: url
