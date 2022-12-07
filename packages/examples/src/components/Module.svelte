9a7542b2ef9a5a6c49c42dff72d52c6a697e2bd1
Nick Jennings <nick@silverbucket.net>
2022-12-07 13:19:59 +0100

    remove Module component

--- packages/examples/src/components/Module.svelte
+++
@@ -1,9 +0,0 @@
-<ContentArea>
-  <div class="md:flex gap-4 w-full">
-    <slot/>
-  </div>
-</ContentArea>
-
-<script>
-  import ContentArea from "./ContentArea.svelte";
-</script>
--- packages/examples/src/routes/xmpp/+page.svelte
+++ packages/examples/src/routes/xmpp/+page.svelte
@@ -3,13 +3,6 @@
   <p>Example for the XMPP platform</p>
 </Intro>
 
-<Module>
-  <div class="w-1/4">
-  <p>Hello World</p>
-  </div>
-</Module>
-
 <script>
   import Intro from "../../components/Intro.svelte";
-  import Module from "../../components/Module.svelte";
 </script>
