d034c84445a8b9dba7bb6cf45efa6b80ad9f2fed
Nick Jennings <nick@silverbucket.net>
2023-04-03 15:37:46 +0200

    fix nav logic

--- packages/examples/src/components/Nav.svelte
+++ packages/examples/src/components/Nav.svelte
@@ -9,15 +9,13 @@
     ["IRC", "/irc"],
     ["XMPP", "/xmpp"],
   ];
+
 </script>
 
 <nav class="flex justify-center space-x-4 p-2">
   {#each navItems as [title, path]}
     <a
-      class="rounded-lg px-2 pt-1 pb-1.5 font-medium {$page.url.pathname
-        .replace(/\/$/, '')
-        .endsWith(path === '/' ? base : path)
-        ? 'bg-sockethub text-blue-50'
+      class="rounded-lg px-2 pt-1 pb-1.5 font-medium {$page.url.pathname.endsWith(path) ? 'bg-sockethub text-blue-50'
         : 'text-slate-700 hover:bg-slate-300'}"
       href="{base}{path}">{title}</a
     >
