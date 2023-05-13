373a1f04d838efa347afea50da4aae2cf2088cbf
Nick Jennings <nick@silverbucket.net>
2023-05-13 16:32:20 +0200

    remove unnecessary delay

--- test/browser.test.js
+++ test/browser.test.js
@@ -232,20 +232,13 @@ describe(`Sockethub tests at port ${SH_PORT}`, () => {
         });
 
         describe("Incoming Message queue", () => {
-            it("should be empty", (done) => {
+            it("should be empty", () => {
                 console.log(
                     `*** MESSAGE QUEUE, length: ${incomingMessages.length} ***`,
                 );
                 console.log(incomingMessages);
-                setTimeout(() => {
-                    console.log(
-                        `*** MESSAGE QUEUE after 500ms delay, length: ${incomingMessages.length} ***`,
-                    );
-                    console.log(incomingMessages);
-                    expect(incomingMessages.length).to.eql(0);
-                    expect(incomingMessages).to.eql([]);
-                    done();
-                }, 500);
+                expect(incomingMessages.length).to.eql(0);
+                expect(incomingMessages).to.eql([]);
             });
         });
     });
