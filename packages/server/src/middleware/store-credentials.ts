4f21518f60500ada90a3329674a8be8efec9396c
Nick Jennings <nick@silverbucket.net>
2023-12-09 00:59:55 +0100

    remove console log

--- packages/server/src/middleware/store-credentials.ts
+++ packages/server/src/middleware/store-credentials.ts
@@ -8,7 +8,6 @@ export default function storeCredentials(store: CredentialsStoreInstance) {
     return (creds: CredentialsObject, done: MiddlewareChainInterface) => {
         try {
             store.save(creds.actor.id, creds).then(() => {
-                console.log("COMPLETED");
                 done(creds);
             });
         } catch (err) {
