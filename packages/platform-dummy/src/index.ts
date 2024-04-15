b98832d0f150a2d90d0165fb0f6a8c7ad55fe497
Nick Jennings <nick@silverbucket.net>
2024-04-15 15:59:28 +0200

    get version

--- packages/platform-dummy/src/index.ts
+++ packages/platform-dummy/src/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable  @typescript-eslint/no-var-requires */
 import {
     ActivityStream,
     Logger,
@@ -8,6 +7,10 @@ import {
     PlatformSchemaStruct,
     PlatformSession,
 } from "@sockethub/schemas";
+
+const packageJSON = await import("../package.json", {
+    assert: { type: "json" }
+};
 
 class Dummy implements PlatformInterface {
     debug: Logger;
@@ -22,7 +25,7 @@ class Dummy implements PlatformInterface {
     get schema(): PlatformSchemaStruct {
         return {
             name: "dummy",
-            version: require("../package.json").version,
+            version: packageJSON.version,
             messages: {
                 required: ["type"],
                 properties: {
