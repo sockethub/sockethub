ce42e11b93007536e24ea0beb2e31b5d8276ce66
Nick Jennings <nick@silverbucket.net>
2024-08-29 15:01:21 +0200

    deno fmt

--- packages/irc2as/README.md
+++ packages/irc2as/README.md
@@ -1,41 +1,46 @@
 # @sockethub/irc2as
 
-Middleware which parses incoming messages from the IRC protocol and converts then to ActivityStreams objects.
+Middleware which parses incoming messages from the IRC protocol and converts
+then to ActivityStreams objects.
 
 ## Overview
 
-Currently, a very simple library to parse incoming IRC protocol messages and generate activity
-streams. The activity streams are not fully AS2.0 compliant, but aim to be shaped in the spirit
-of them, and as time goes on hopefully become more compliant (PRs & feedback welcome).
+Currently, a very simple library to parse incoming IRC protocol messages and
+generate activity streams. The activity streams are not fully AS2.0 compliant,
+but aim to be shaped in the spirit of them, and as time goes on hopefully become
+more compliant (PRs & feedback welcome).
 
 ## Usage
 
 ```javascript
-    var IRC2AS = require('irc2as');
-    var irc2as = new IRC2AS({server: 'irc.freenode.net'});
+var IRC2AS = require("irc2as");
+var irc2as = new IRC2AS({ server: "irc.freenode.net" });
 
-    irc2as.events.on('incoming', function (asObject) {
-      console.log('activity stream: ', asObject);
-    });
+irc2as.events.on("incoming", function (asObject) {
+  console.log("activity stream: ", asObject);
+});
 
-    irc2as.events.on('error', function (asObject) {
-      console.log('error response to something we sent: ', asObject);
-    });
+irc2as.events.on("error", function (asObject) {
+  console.log("error response to something we sent: ", asObject);
+});
 
-    irc2as.events.on('unprocessed', function (line) {
-      console.log(`irc line we don't know what to do with (yet), PRs welcome`, line);
-    });
+irc2as.events.on("unprocessed", function (line) {
+  console.log(
+    `irc line we don't know what to do with (yet), PRs welcome`,
+    line,
+  );
+});
 
-    irc2as.events.on('pong', function (timestamp) {
-      console.log('confirmation of something we sent: ', timestamp);
-    });
+irc2as.events.on("pong", function (timestamp) {
+  console.log("confirmation of something we sent: ", timestamp);
+});
 
-    irc2as.events.on('ping', function (timestamp) {
-      console.log('sending ping to server at ', timestamp);
-    });
+irc2as.events.on("ping", function (timestamp) {
+  console.log("sending ping to server at ", timestamp);
+});
 
-    // ....  some code to get IRC socket messages
-    ircClient.on('data', this.irc2as.input.bind(this.irc2as));
+// ....  some code to get IRC socket messages
+ircClient.on("data", this.irc2as.input.bind(this.irc2as));
 ```
 
 ## License
--- packages/irc2as/src/index.test.js
+++ packages/irc2as/src/index.test.js
@@ -9,113 +9,114 @@ const ircData = fs.readFileSync("./src/index.data.irc.test.txt", "utf-8");
 const inputs = ircData.split("\n");
 
 function timeout(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 await timeout(0);
 
 function matchStream() {
-    return async (stream) => {
-        return new Promise((resolve, reject) => {
-            assertEquals(typeof stream.published, "string");
-            delete stream.published;
-            let matched = false;
-            for (let i = 0; i <= outputs.length; i++) {
-                matched = equal(stream, outputs[i]);
-                if (matched) {
-                    // when matched, remove output entry from list
-                    outputs.splice(i, 1);
-                    break;
-                }
-            }
-            if (!matched) {
-                console.log();
-                console.log("available matches:" + JSON.stringify(outputs));
-                console.log(
-                    "failed to find match for: " + JSON.stringify(stream),
-                );
-                reject(new Error("failed matching " + JSON.stringify(stream)));
-            }
-            const err = schemas.validateActivityStream(stream);
-            if (err) {
-                reject(new Error(err + " - " + JSON.stringify(stream)));
-            }
-            resolve();
-        });
-    };
+  return async (stream) => {
+    return new Promise((resolve, reject) => {
+      assertEquals(typeof stream.published, "string");
+      delete stream.published;
+      let matched = false;
+      for (let i = 0; i <= outputs.length; i++) {
+        matched = equal(stream, outputs[i]);
+        if (matched) {
+          // when matched, remove output entry from list
+          outputs.splice(i, 1);
+          break;
+        }
+      }
+      if (!matched) {
+        console.log();
+        console.log("available matches:" + JSON.stringify(outputs));
+        console.log(
+          "failed to find match for: " + JSON.stringify(stream),
+        );
+        reject(new Error("failed matching " + JSON.stringify(stream)));
+      }
+      const err = schemas.validateActivityStream(stream);
+      if (err) {
+        reject(new Error(err + " - " + JSON.stringify(stream)));
+      }
+      resolve();
+    });
+  };
 }
 
 Deno.test("IRC2AS", async () => {
-    const irc2as = new IRC2AS({ server: "localhost" });
-    assertEquals(!!irc2as.events, true);
-    assertEquals(!!irc2as.input, true);
-    assertEquals(typeof irc2as.events.on, "function");
+  const irc2as = new IRC2AS({ server: "localhost" });
+  assertEquals(!!irc2as.events, true);
+  assertEquals(!!irc2as.input, true);
+  assertEquals(typeof irc2as.events.on, "function");
 
-    irc2as.events.on("unprocessed", (string) => {
-        console.log("unprocessed> " + string);
-    });
+  irc2as.events.on("unprocessed", (string) => {
+    console.log("unprocessed> " + string);
+  });
 
-    irc2as.events.removeAllListeners();
+  irc2as.events.removeAllListeners();
 });
 
 Deno.test("inputs generate expected outputs", async () => {
-    const irc2as = new IRC2AS({ server: "localhost" });
-    let pongs = 0,
-        pings = 0;
-    let callbackCalled = false;
+  const irc2as = new IRC2AS({ server: "localhost" });
+  let pongs = 0,
+    pings = 0;
+  let callbackCalled = false;
 
-    const callback = async (event) => {
-        const match = await matchStream();
-        callbackCalled = true;
-        await match(event);
-    };
+  const callback = async (event) => {
+    const match = await matchStream();
+    callbackCalled = true;
+    await match(event);
+  };
 
-    irc2as.events.on("incoming", callback);
-    irc2as.events.on("error", callback);
-    irc2as.events.on("pong", () => {
-        pongs++;
-    });
-    irc2as.events.on("ping", () => {
-        pings++;
-    });
+  irc2as.events.on("incoming", callback);
+  irc2as.events.on("error", callback);
+  irc2as.events.on("pong", () => {
+    pongs++;
+  });
+  irc2as.events.on("ping", () => {
+    pings++;
+  });
 
-    for (let i = 0; inputs.length > i; i++) {
-        irc2as.input(inputs[i]);
-    }
+  for (let i = 0; inputs.length > i; i++) {
+    irc2as.input(inputs[i]);
+  }
 
-    await timeout(0);
-    assertEquals(callbackCalled, true);
-    assertEquals(outputs.length, 0);
-    assertEquals(pings, 2);
-    assertEquals(pongs, 3);
+  await timeout(0);
+  assertEquals(callbackCalled, true);
+  assertEquals(outputs.length, 0);
+  assertEquals(pings, 2);
+  assertEquals(pongs, 3);
 
-    irc2as.events.removeAllListeners();
+  irc2as.events.removeAllListeners();
 });
 
 Deno.test("handle many room joins", async () => {
-    const irc2as = new IRC2AS({ server: "localhost" });
-    let sentCount = 0;
-    let receivedCount = 0;
+  const irc2as = new IRC2AS({ server: "localhost" });
+  let sentCount = 0;
+  let receivedCount = 0;
 
-    irc2as.events.on("incoming", (d) => {
-        receivedCount += 1;
-    });
+  irc2as.events.on("incoming", (d) => {
+    receivedCount += 1;
+  });
 
-    for (let i = 0; i < 5; i++) {
-        let names =
-            ":hitchcock.freenode.net 353 hyper_slvrbckt @ #kosmos-random :hyper_slvrbckt ";
-        for (let n = 0; n < 100; n++) {
-            names += ` hal8000${i}${n} botka${i}${n} gregkare${i}${n} raucao${i}${n} galfert${i}${n}`;
-        }
-        sentCount += 1;
-        irc2as.input(names);
+  for (let i = 0; i < 5; i++) {
+    let names =
+      ":hitchcock.freenode.net 353 hyper_slvrbckt @ #kosmos-random :hyper_slvrbckt ";
+    for (let n = 0; n < 100; n++) {
+      names +=
+        ` hal8000${i}${n} botka${i}${n} gregkare${i}${n} raucao${i}${n} galfert${i}${n}`;
     }
-    irc2as.input(
-        ":hitchcock.freenode.net 366 hyper_slvrbckt #kosmos-random :End of /NAMES list.",
-    );
+    sentCount += 1;
+    irc2as.input(names);
+  }
+  irc2as.input(
+    ":hitchcock.freenode.net 366 hyper_slvrbckt #kosmos-random :End of /NAMES list.",
+  );
 
-    await timeout(0);
-    assertEquals(sentCount, 5);
-    assertEquals(receivedCount, 5 * 100 * sentCount + sentCount);
+  await timeout(0);
+  assertEquals(sentCount, 5);
+  assertEquals(receivedCount, 5 * 100 * sentCount + sentCount);
 
-    irc2as.events.removeAllListeners();
+  irc2as.events.removeAllListeners();
 });
