3b8ab155f6bfc3fd7d694096743c47adaffa02d2
Nick Jennings <nick@silverbucket.net>
2025-02-02 17:31:48 +0100

    updated eslint config

---
+++ packages/examples/eslint.config.mjs
@@ -0,0 +1,65 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import parser from "svelte-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    {
+        ignores: ["build", ".svelte-kit"],
+    },
+    ...compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:svelte/recommended",
+        "prettier",
+    ),
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 2020,
+            sourceType: "module",
+
+            parserOptions: {
+                extraFileExtensions: [".svelte"],
+            },
+        },
+
+        rules: {
+            "svelte/no-at-html-tags": "off",
+        },
+    },
+    {
+        files: ["**/*.svelte"],
+
+        languageOptions: {
+            parser: parser,
+            ecmaVersion: 5,
+            sourceType: "script",
+
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+            },
+        },
+    },
+];
