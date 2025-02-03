34e8e14850807f7aad9fc08b41cdf74b9a4af09c
Nick Jennings <nick@silverbucket.net>
2025-02-03 02:10:17 +0100

    improve tests

--- packages/server/src/middleware/create-activity-object.test.ts
+++ packages/server/src/middleware/create-activity-object.test.ts
@@ -1,11 +1,18 @@
-import { describe, it } from "bun:test";
+import { describe, it, expect } from "bun:test";
 import createActivityObject from "./create-activity-object.js";
 
 describe("Middleware: createActivityObject", () => {
+    it("Calls activity.Object.create and fails with invalid property", (done) => {
+        expect(() => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            createActivityObject({ foo: "bar" }, () => {
+                done();
+            });
+        }).toThrow("invalid property: \"foo\"");
+    });
     it("Calls activity.Object.create with incoming data", (done) => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        createActivityObject({ foo: "bar" }, () => {
+        createActivityObject({ context: "foo", type: "bar", actor: {type: "person", id: "some actor"} }, () => {
             done();
         });
     });
