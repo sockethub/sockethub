da9c897e6da20749fe666d91435ed015e92902ae
Nick Jennings <nick@silverbucket.net>
2025-02-25 01:20:31 +0100

    fix unit tests

--- packages/platform-feeds/src/index.test.ts
+++ packages/platform-feeds/src/index.test.ts
@@ -25,8 +25,22 @@ describe("platform-feeds", () => {
                 id: "some url"
             }
         }, (err, results: ASCollection) => {
-            expect(results.totalItems).toEqual(10);
-            expect(results.items[5].actor.name).toEqual("sub-fish");
+            expect(results['totalItems']).toEqual(20);
+            expect(results['items'][5]['object']).toEqual({
+                type: "article",
+                title: "Sockethub 3.x",
+                id: "https://sockethub.org/news/2019-09-26-3x.html",
+                brief: undefined,
+                content: "<p>Sockethub 3.0 has been released and includes a lot of improvements focusing mainly on XMPP and IRC, additionally a ton of internal improvements. Ongoing releases tracked on the <a href=\"https://github.com/sockethub/sockethub/releases\">Github page</a>. </p>",
+                contentType: "html",
+                url: "https://sockethub.org/news/2019-09-26-3x.html",
+                published: "2019-09-26T00:00:00.000Z",
+                updated: undefined,
+                datenum: 1569456000000,
+                tags: undefined,
+                media: undefined,
+                source: undefined,
+            })
         })
     })
 })
