2ef45b3a892707181e0575bd742e25c0b477398b
Nick Jennings <nick@silverbucket.net>
2025-05-20 22:30:01 +0200

    New Platform: Metadata (#902)
    * added metadata platform
    
    * improved docker build
    
    * show orange or green circle next to logs to indicate response
    
    * remove actor for dummy platform example

--- packages/examples/src/components/logs/LogEntry.svelte
+++ packages/examples/src/components/logs/LogEntry.svelte
@@ -2,23 +2,25 @@
 import type { AnyActivityStream } from "$lib/sockethub";
 import DummyEntry from "./platforms/DummyEntry.svelte";
 import FeedsEntry from "./platforms/FeedsEntry.svelte";
-import GenericEntry from "./platforms/IrcEntry.svelte";
+import GenericEntry from "./platforms/GenericEntry.svelte";
 interface Props {
     id: string;
     entry: AnyActivityStream;
     buttonAction: () => void;
+    response: boolean;
 }
 
-let { id, entry, buttonAction }: Props = $props();
+let { id, entry, buttonAction, response }: Props = $props();
 </script>
 
 <li>
     <button
         onclick={buttonAction}
         data-modal-toggle="defaultModal"
-        class="hover:bg-blue-400 bg-blue-300 text-black py-0 px-2 rounded mr-3 mb-1"
+        class="hover:bg-blue-400 bg-blue-300 text-black py-0 px-2 rounded mb-1"
         >view log</button
     >
+    <span class="{ response ? 'bg-green-500' : 'bg-orange-300'} rounded-full pl-2 pr-2 mr-1"></span>
     {#if entry.context === "dummy"}
         <DummyEntry {id} {entry} />
     {:else if entry.context === "feeds"}
