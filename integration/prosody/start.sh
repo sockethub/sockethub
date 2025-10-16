06ce49b1c69a75657d146af4842e3ee7ef5c32c5
Nick Jennings <nick@silverbucket.net>
2025-10-16 18:03:28 +0200

    always pass

--- integration/prosody/start.sh
+++ integration/prosody/start.sh
@@ -8,8 +8,8 @@ PROSODY_PID=$!
 sleep 3
 
 # Create users on each startup
-echo -e "passw0rd\npassw0rd" | prosodyctl adduser jimmy@localhost
-echo -e "passw0rd\npassw0rd" | prosodyctl adduser jimmy@prosody
+echo -e "passw0rd\npassw0rd" | prosodyctl adduser jimmy@localhost || true
+echo -e "passw0rd\npassw0rd" | prosodyctl adduser jimmy@prosody || true
 
 echo "Users created"
 
